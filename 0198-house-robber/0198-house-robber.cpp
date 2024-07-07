a49efcc26d266ae39e6ff0d72da11ea81d28a78f
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-07-07 15:29:38 +0530

    Time: 4 ms (31.36%), Space: 9.3 MB (10.68%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int solveusingrec(vector<int>& nums, int index) {
+        // base case
+        if (index >= nums.size()) {
+            return 0;
+        }
+        int include = nums[index] + solveusingrec(nums, index + 2);
+        int exclude = solveusingrec(nums, index + 1);
+        return max(include, exclude);
+    }
+
+    int solvebyMemoisation(vector<int>& nums, int index, vector<int>& dp) {
+        if (index >= nums.size()) {
+            return 0;
+        }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int include = nums[index] + solvebyMemoisation(nums, index + 2, dp);
+        int exclude = solvebyMemoisation(nums, index + 1, dp);
+        dp[index] = max(include, exclude);
+        return dp[index];
+    }
+
+    int rob(vector<int>& nums) {
+        int index = 0;
+        
+        // int ans = solveusingrec(nums, index);
+        // return ans;
+
+        // Memoization approach
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        int ans = solvebyMemoisation(nums, index, dp);
+        return ans;
+    }
+};
