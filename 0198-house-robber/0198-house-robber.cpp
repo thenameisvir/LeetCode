ca49ce9af13e6edc2990547b388bd072eaa82bb5
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-07-10 12:51:59 +0530

    Time: 4 ms (31.31%), Space: 9.2 MB (35.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,55 +1,35 @@
 class Solution {
 public:
-    int solveusingrec(vector<int>& nums, int index) {
-        // base case
-        if (index >= nums.size()) {
-            return 0;
-        }
-        int include = nums[index] + solveusingrec(nums, index + 2);
-        int exclude = solveusingrec(nums, index + 1);
-        return max(include, exclude);
-    }
+    int solvebyrecursion(vector<int>&nums,int index){
+        if(index>=nums.size()) return 0;
 
-    int solvebyMemoisation(vector<int>& nums, int index, vector<int>& dp) {
-        if (index >= nums.size()) {
-            return 0;
-        }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        int include = nums[index] + solvebyMemoisation(nums, index + 2, dp);
-        int exclude = solvebyMemoisation(nums, index + 1, dp);
-        dp[index] = max(include, exclude);
+        // logic of include
+        int include = nums[index] + solvebyrecursion(nums,index+2);
+        // logic of exclude
+        int exclude = 0 + solvebyrecursion(nums,index+1);
+
+        int pa = max(include,exclude);
+        return pa;
+    }
+    int solveebymemoisation(vector<int>& nums,int index,vector<int>&dp){
+        if(index>=nums.size()) return 0;
+
+        if(dp[index]!=-1) return dp[index];
+
+        // logic of include
+        int include = nums[index] + solveebymemoisation(nums,index+2,dp);
+        // logic of exclude
+        int exclude = 0 + solveebymemoisation(nums,index+1,dp);
+
+        dp[index] = max(include,exclude);
+
         return dp[index];
-    }
-    // tabulation
-    int solveusingtab(vector<int>& nums){
-        int n = nums.size();
-        vector<int> dp(n, -1);
 
-        dp[n-1] = nums[n-1];
-        for(int index = n-2;index>=0;index--){
-            int tempans = 0;
-            if(index+2<n){
-                tempans = dp[index+2];
-            }
-            
-            int include = nums[index] + tempans;
-            int exclude = dp[index+1];
-            dp[index] = max(include, exclude);
-        }
-        return dp[0];
     }
     int rob(vector<int>& nums) {
         int index = 0;
-        
-        // int ans = solveusingrec(nums, index);
-        // return ans;
-
-        // Memoization approach
-        int n = nums.size();
-        // vector<int> dp(n, -1);
-        int ans = solveusingtab(nums);
+        vector<int>dp(nums.size(),-1);
+        int ans = solveebymemoisation(nums,index,dp);
         return ans;
     }
 };
