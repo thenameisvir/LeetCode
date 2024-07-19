1daf731d0d4d91339d5885a61e6b872f49b319a7
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-07-19 14:26:29 +0530

    Time: 8 ms (9.6%), Space: 13.9 MB (8.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,55 +1,118 @@
+// class Solution {
+// public:
+//     int findpivot(vector<int>& arr){
+//         int s = 0;
+//         int n = arr.size();
+//         int e = n-1;
+//         while(s<=e){
+//             int m = s+(e-s)/2;
+//             if(m+1<n && arr[m]>arr[m+1]){
+//                 return m;
+//             }
+//             else if(m-1>=0 && arr[m-1]>arr[m]){
+//                 return m-1;
+//             }
+//             else if(arr[m]>=arr[s]){
+//                 s = m+1;
+//             }
+//             else{
+//                 e = m-1;
+//             }
+
+//         }
+//         return -1;
+//     }
+//     int binary(vector<int>& arr,int s,int e,int target){
+//         while(s<=e){
+//             int m = s+(e-s)/2;
+//             if(arr[m]==target){
+//                 return m;
+//             }
+//             else if(arr[m]>target){
+//                 e = m-1;
+//             }
+//             else{
+//                 s = m+1;
+//             }
+//         }
+//         return -1;
+//     }
+//     int search(vector<int>& arr, int target) {
+//         int index = findpivot(arr);
+//         int ans = -1;
+//         int last = arr.size();
+//         if(target>=arr[0] && target<=arr[index]){
+//             ans = binary(arr,0,index,target);
+//         }
+//         else{
+//             ans = binary(arr,index+1,last-1,target);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    // Function to find the index of the pivot element
-    int pivot(const vector<int>& arr) {
+    int findpivot(vector<int>& arr){
         int s = 0;
-        int e = arr.size() - 1;
         int n = arr.size();
-        
-        while (s < e) {
+        int e = n-1;
+        while(s<=e){
             int m = s + (e - s) / 2;
-            if (arr[m] > arr[e]) {
-                s = m + 1;
-            } else {
-                e = m;
-            }
-        }
-        return s; // 's' is the index of the smallest element, i.e., the pivot
-    }
-
-    // Function to perform binary search on the subarray
-    int binarysearch(const vector<int>& arr, int s, int e, int target) {
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (arr[m] == target) {
+            if(m+1 < n && arr[m] > arr[m+1]){
                 return m;
-            } else if (arr[m] > target) {
-                e = m - 1;
-            } else {
-                s = m + 1;
+            }
+            else if(m-1 >= 0 && arr[m-1] > arr[m]){
+                return m-1;
+            }
+            else if(arr[m] >= arr[s]){
+                s = m+1;
+            }
+            else{
+                e = m-1;
             }
         }
-        return -1; // Element not found
+        return -1;
     }
 
-    // Main function to search for the target in the rotated sorted array
+    int binary(vector<int>& arr, int s, int e, int target){
+        while(s <= e){
+            int m = s + (e - s) / 2;
+            if(arr[m] == target){
+                return m;
+            }
+            else if(arr[m] > target){
+                e = m-1;
+            }
+            else{
+                s = m+1;
+            }
+        }
+        return -1;
+    }
+
     int search(vector<int>& arr, int target) {
         int n = arr.size();
-        if (n == 0) return -1; // Empty array
-        
-        int pivotIndex = pivot(arr);
+        if (n == 0) return -1;
 
-        // If the array is not rotated
-        if (pivotIndex == 0) {
-            return binarysearch(arr, 0, n - 1, target);
-        }
-        
-        // If target is within the left part of the array
-        if (target >= arr[0] && target <= arr[pivotIndex - 1]) {
-            return binarysearch(arr, 0, pivotIndex - 1, target);
+        int pivot = findpivot(arr);
+
+        // If array is not rotated
+        if (pivot == -1) {
+            return binary(arr, 0, n-1, target);
         }
 
-        // If target is within the right part of the array
-        return binarysearch(arr, pivotIndex, n - 1, target);
+        // If the target is the pivot element
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+
+        // Determine which part of the array to search in
+        if(target >= arr[0] && target <= arr[pivot]){
+            return binary(arr, 0, pivot, target);
+        }
+        else{
+            return binary(arr, pivot+1, n-1, target);
+        }
     }
 };
