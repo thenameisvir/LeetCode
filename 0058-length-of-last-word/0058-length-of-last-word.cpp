7bdba1ebade3b9b331995535626188259de86e33
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-01 09:12:50 +0530

    Time: 0 ms (100%), Space: 8.1 MB (8.76%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,31 +1,29 @@
-#include <string>
-using namespace std;
-
 class Solution {
 public:
-    int find(const string& s){
-        int num = 0;
+    int find(string s){
         int sum = 0;
+        int num = 0;
         int i = 0;
         int n = s.length();
-        
-        while (i < n) {
-            if (s[i] != ' ') {
+        while(i<n){
+            if(s[i]!=' '){
                 num++;
-            } else if (s[i] == ' ') {
-                if (num > 0) {
+            }
+            else if(s[i]==' '){
+                if(num>0){
                     sum = num;
                 }
                 num = 0;
+
             }
             i++;
         }
-        if (num > 0) { // For the last word
+
+        if(num>0){
             sum = num;
         }
         return sum;
     }
-    
     int lengthOfLastWord(string s) {
         return find(s);
     }
