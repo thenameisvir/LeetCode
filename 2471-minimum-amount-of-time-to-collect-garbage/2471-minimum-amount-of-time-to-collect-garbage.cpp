3970fc299ec0406d7ea39092680f6eea912a1176
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-05 10:29:34 +0530

    Time: 162 ms (42.84%), Space: 106 MB (29.34%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+    int garbageCollection(vector<string>& arr, vector<int>& travel) {
         int pickP = 0;
         int pickM = 0;
         int pickG = 0;
-
-        int travelP = 0;
-        int travelM = 0;
-        int travelG = 0;
 
         int lastP = 0;
         int lastM = 0;
         int lastG = 0;
 
-        for(int i=0;i<garbage.size();i++){
-            string str = garbage[i];
-            for(int j=0;j<str.length();j++){
-                char ch = str[j];
-                if(ch=='G'){
-                    pickG++;
-                    lastG = i;
+        int travelP = 0;
+        int travelM = 0;
+        int travelG = 0;
+
+        for(int i=0;i<arr.size();i++){
+            string curr = arr[i];
+            for(int j=0;j<curr.length();j++){
+                if(curr[j]=='P'){
+                pickP++;
+                lastP = i;
                 }
-                if(ch=='M'){
-                    pickM++;
-                    lastM = i;
+                if(curr[j]=='M'){
+                pickM++;
+                lastM = i;
                 }
-                if(ch=='P'){
-                    pickP++;
-                    lastP = i;
+                if(curr[j]=='G'){
+                pickG++;
+                lastG = i;
                 }
             }
         }
 
-        for(int i=0;i<lastP;i++){
-            travelP+=travel[i];
-        }
         for(int i=0;i<lastM;i++){
             travelM+=travel[i];
         }
         for(int i=0;i<lastG;i++){
             travelG+=travel[i];
         }
+        for(int i=0;i<lastP;i++){
+            travelP+=travel[i];
+        }
 
-        int ans = (pickP+travelP)+(pickM+travelM)+(pickG+travelG);
+        int ans = (pickP+pickM+pickG) + (travelP+travelM+travelG);
         return ans;
     }
 };
