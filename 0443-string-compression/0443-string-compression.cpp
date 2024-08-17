55fe298f2fdde220ee9d06730580230581f36003
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-18 00:25:54 +0530

    Time: 4 ms (68.06%), Space: 14.1 MB (7.64%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -7,22 +7,23 @@ public:
             char curr = c[i];
             int count = 0;
 
-            while(i<c.size() && c[i]==curr){
-                i++;
+            while(i<c.size()&& c[i]==curr){
                 count++;
+                i++;
             }
 
             ans.push_back(curr);
 
             if(count>1){
                 string str = to_string(count);
-                for(char ch: str){
+                for(auto ch:str){
                     ans.push_back(ch);
                 }
             }
-
         }
+
         c = ans;
-        return ans.size();
+        return c.size();
     }
+
 };
