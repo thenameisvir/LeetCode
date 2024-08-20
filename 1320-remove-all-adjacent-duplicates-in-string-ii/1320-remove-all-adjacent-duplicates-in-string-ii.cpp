962dc5808fa453a048857835103e0a4ded65d15e
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-20 15:03:46 +0530

    Time: 2016 ms (5%), Space: 10.8 MB (89.44%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool arekminusone(char&ch,string&ans,int k_1){
+
+    bool kminusone(char&ch,string&ans,int k_1){
         int it = ans.size()-1;
         for(int i=0;i<k_1;i++){
             if(ch!=ans[it]) return false;
             it--;
         }
-
         return true;
     }
     string removeDuplicates(string s, int k) {
         string ans = "";
         for(int i=0;i<s.size();i++){
             char&ch = s[i];
-            if(ans.size()<k-1){
+            if(ans.length()<k-1){
                 ans.push_back(ch);
             }
             else{
-                if(arekminusone(ch,ans,k-1)){
+                if(kminusone(ch,ans,k-1)){
                     for(int j=0;j<k-1;j++){
                         ans.pop_back();
                     }
