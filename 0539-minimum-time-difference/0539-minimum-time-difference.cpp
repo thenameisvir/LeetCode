adb9dbdb1e180fbd6bb084710b5b496f72533b3f
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-23 12:14:02 +0530

    Time: 10 ms (77.61%), Space: 17.7 MB (32.58%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        vector<int> v;
-        for (const auto& time : timePoints) {
-            int hours = stoi(time.substr(0, 2));
-            int minutes = stoi(time.substr(3, 2));
-            int total = hours * 60 + minutes;
+        vector<int>v;
+        for(int i=0;i<timePoints.size();i++){
+            string time = timePoints[i];
+            int hours = std::stoi(time.substr(0,2));
+            int minutes = std::stoi(time.substr(3,2));
+            int total = hours*60 + minutes;
             v.push_back(total);
         }
-        
-        // Sort the time in minutes
-        sort(v.begin(), v.end());
 
-        int minDiff = INT_MAX;
-        for (int i = 1; i < v.size(); i++) {
-            minDiff = min(minDiff, v[i] - v[i-1]);
+        sort(v.begin(),v.end());
+        int mindiff = INT_MAX;
+        for(int i = 1;i<v.size();i++){
+            mindiff = min(mindiff , v[i] - v[i-1]);
         }
 
-        // Also consider the difference between the first and last time (across midnight)
-        int firstLastDiff = v[0] + 1440 - v.back();
-        minDiff = min(minDiff, firstLastDiff);
+        int first = v[0] + 1440 -v.back();
+        mindiff = min(mindiff,first);
 
-        return minDiff;
+        return mindiff;
     }
 };
