ebf50f3afe228eb6e468dd8a1dd7116ab00d3436
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-23 13:03:02 +0530

    Time: 86 ms (67.04%), Space: 27.2 MB (49.09%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int>v;
-        for(const string& str : bank){
+        vector<int> v;
+        for (auto& str : bank) {  // auto deduces the type of str as string&
             int total = count(str.begin(), str.end(), '1');
             if (total > 0) {
                 v.push_back(total);
@@ -10,8 +10,8 @@ public:
         }
 
         long long int total_sum = 0;
-        for(int i = 1; i < v.size(); i++) {
-            total_sum += v[i] * v[i-1];
+        for (size_t i = 1; i < v.size(); ++i) {
+            total_sum += v[i] * v[i - 1];
         }
 
         return total_sum;
