c9a2fbfac1a6b6295b5399158016501a1db4edf1
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-23 12:23:57 +0530

    Time: 13 ms (53.47%), Space: 17.7 MB (32.58%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -6,19 +6,20 @@ public:
             string time = timePoints[i];
             int hours = std::stoi(time.substr(0,2));
             int minutes = std::stoi(time.substr(3,2));
+
             int total = hours*60 + minutes;
+
             v.push_back(total);
         }
-
         sort(v.begin(),v.end());
+
         int mindiff = INT_MAX;
-        for(int i = 1;i<v.size();i++){
-            mindiff = min(mindiff , v[i] - v[i-1]);
+        for(int i=1;i<v.size();i++){
+            mindiff = min(mindiff, v[i] - v[i-1]);
         }
 
-        int first = v[0] + 1440 -v.back();
+        int first = (v[0] + 1440) - v.back();
         mindiff = min(mindiff,first);
-
         return mindiff;
     }
 };
