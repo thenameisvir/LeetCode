4431ba06b81033ad5f3fda38adb5685901fce686
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-26 15:21:11 +0530

    Time: 14 ms (86.64%), Space: 12.5 MB (59.02%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-         string ans = "";
-        int i = 0;
-        while(i<s.length()){
-            if(ans.length()>0 && ans[ans.length()-1]==s[i]){
+        string ans = "";
+        for(int i=0;i<s.length();i++){
+            if(!ans.empty() && ans[ans.length()-1]==s[i]){
                 ans.pop_back();
             }
             else{
                 ans.push_back(s[i]);
             }
-            i++;
         }
+
         return ans;
     }
 };
