aee70008f5a828081a68c5ccb55f0b650771d079
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-08-27 20:05:26 +0530

    Time: 2189 ms (5.06%), Space: 10.8 MB (92.99%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    bool kminusone(char& ch, string& ans, int k_1) {
-        int it = ans.length() - 1;
-        for (int i = 0; i < k_1; i++) {
-            if (ans[it] != ch) return false;
+
+    bool kminusone(char&ch,string&ans,int k_1){
+        int it = ans.length()-1;
+        for(int i=0;i<k_1;i++){
+            if(ans[it]!=ch) return false;
             it--;
         }
         return true;
     }
-
     string removeDuplicates(string s, int k) {
         string ans = "";
-        for (int i = 0; i < s.length(); i++) {
-            char& ch = s[i];
-            ans.push_back(ch);
-            if (ans.length() >= k && kminusone(ch, ans, k - 1)) {
-                for (int j = 0; j < k; j++) {
-                    ans.pop_back();
+        for(int i=0;i<s.length();i++){
+            char&ch = s[i];
+            if(ans.length()<k-1){
+                ans.push_back(ch);
+            }
+            else{
+                if(kminusone(ch,ans,k-1)){
+                    for(int i=0;i<k-1;i++){
+                        ans.pop_back();
+                    }
+                }
+                else{
+                    ans.push_back(ch);
                 }
             }
         }
+
         return ans;
     }
 };
