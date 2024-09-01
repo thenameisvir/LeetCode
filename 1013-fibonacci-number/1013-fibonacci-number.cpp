6842ff54ef68216324b458d3fdbe8a1b495f30bb
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-09-01 18:06:49 +0530

    Time: 7 ms (58.08%), Space: 7.4 MB (67.34%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    int SolveusingMemo(int n, vector<int>& dp) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-
-        dp[n] = SolveusingMemo(n-1, dp) + SolveusingMemo(n-2, dp);
-        return dp[n];
-    }
-
     int fib(int n) {
-        if (n < 2) return n; 
-        vector<int> dp(n + 1, -1);
-        int ans = SolveusingMemo(n, dp);
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        int ans = fib(n-1) + fib(n-2);
         return ans;
     }
 };
