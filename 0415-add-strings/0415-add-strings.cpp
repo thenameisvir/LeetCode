3e3fdf886f2fc1394bab3a5cb5ecfe1634c25e1b
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-09-05 23:22:21 +0530

    Time: 5 ms (49.76%), Space: 8.3 MB (96.15%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    void addRe(string&num1, string&num2,int p1,int p2,int carry,string&ans){
+        // base case
+        if(p1<0 && p2<0){
+            if(carry!=0){
+                ans.push_back(carry+'0');
+            }
+            return;
+        }
+
+
+        // ek case solve
+        int n1 = (p1>=0 ? num1[p1] : '0') -'0';
+        int n2 = (p2>=0 ? num2[p2] : '0') -'0';
+        int csum = n1 + n2 + carry;
+        int digit = csum%10;
+         carry = csum/10;
+        ans.push_back(digit+'0');
+
+        // Rec
+        addRe(num1,num2,p1-1,p2-1,carry,ans);
+
+
+    }
+    string addStrings(string num1, string num2) {
+        string ans = "";
+        addRe(num1,num2,num1.size()-1,num2.size()-1,0,ans);
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+
+
+};
