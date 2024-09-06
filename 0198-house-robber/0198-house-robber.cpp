308b5d836395929296237d1fb2b535c3fe41bc24
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-09-07 00:13:59 +0530

    Time: 4 ms (31.97%), Space: 9.2 MB (91.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,16 @@
-class Solution {  
-public:  
-    int rob(vector<int>& arr) {  
-        if (arr.empty()) return 0;  
-        if (arr.size() == 1) return arr[0];  
+class Solution {
+public:
+    int rob(vector<int>& arr) {
+        if(arr.empty()) return 0;
+        if(arr.size()==1) return arr[0];
+        int prev1 = 0;
+        int prev2 = 0;
 
-        int prev1 = 0; // Max amount robbed up to the previous house  
-        int prev2 = 0; // Max amount robbed up to the house before the previous one  
-
-        for (int i = 0; i < arr.size(); i++) {  
-            int current = max(prev2 + arr[i], prev1); // Choose to rob current house or not  
-            prev2 = prev1; // Move prev1 to prev2  
-            prev1 = current; // Update prev1 to current  
-        }  
-
-        return prev1; // The last computed value is the maximum amount that can be robbed  
-    }  
+        for(int i=0;i<arr.size();i++){
+            int current = max(prev2+arr[i],prev1);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
 };
