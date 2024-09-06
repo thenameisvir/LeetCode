157575e57b1e06d803829b4dcc9e9309734e8599
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-09-07 00:18:58 +0530

    Time: 4 ms (31.97%), Space: 9.1 MB (96.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+    int chori(vector<int>& arr,int prev1,int prev2,int index){
+        if(index>=arr.size()){
+            return prev1;
+        }
+
+        int current = max(arr[index] + prev2,prev1);
+        prev2 = prev1;
+        prev1 = current;
+
+        return chori(arr,prev1,prev2,index+1);
+
+
+    }
     int rob(vector<int>& arr) {
-        if(arr.empty()) return 0;
         if(arr.size()==1) return arr[0];
+        if(arr.empty()) return 0;
         int prev1 = 0;
         int prev2 = 0;
-
-        for(int i=0;i<arr.size();i++){
-            int current = max(prev2+arr[i],prev1);
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return prev1;
+        int index = 0;
+        int ans = chori(arr,prev1,prev2,index);
+        return ans;
     }
 };
