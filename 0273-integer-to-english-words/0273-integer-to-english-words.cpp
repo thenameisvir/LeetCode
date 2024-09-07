c74cc94c2122f238e437e1f30f43877b38fe5a85
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-09-07 15:08:58 +0530

    Time: 3 ms (66.52%), Space: 12.3 MB (32.98%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -8,25 +8,25 @@ public:
         {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"}, {9, "Nine"}, {8, "Eight"},
         {7, "Seven"}, {6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"}, {2, "Two"}, {1, "One"}
     };
-
     string numberToWords(int num) {
-        if (num == 0) return "Zero";
-
-        for (auto it : mp) {
-            if (num >= it.first) {
+        if(num==0) return "Zero";
+        for(auto str:mp){
+            if(num>=str.first){
                 string a = "";
-                if (num >= 100) {
-                    a = numberToWords(num / it.first) + " ";
+                if(num>=100){
+                    a = numberToWords(num/str.first)+ " ";
                 }
-                string b = it.second;
+                string b = str.second;
                 string c = "";
-                if (num % it.first != 0) {
-                    c = " " + numberToWords(num % it.first);
+                if(num%str.first!=0){
+                    c = " " + numberToWords(num%str.first);
                 }
-
-                return a + b + c;
+                return a+b+c;
             }
+
+            
         }
+
         return "";
     }
 };
