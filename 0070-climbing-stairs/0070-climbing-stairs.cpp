7a8eebfb86f06d878a9dc7366de7ce366f7e7d35
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-09-15 15:51:30 +0530

    Time: 0 ms (100%), Space: 7.4 MB (90.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int climb(int n,int a,int b,int c,int i){
+        if(i>=n) return c;
+
+        c = a+b;
+        a = b;
+        b = c;
+        return climb(n,a,b,c,i+1);
+    }
     int climbStairs(int n) {
-        int a = 0,b = 1,c;
-        for(int i=0;i<n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return b;
+        int a = 0;
+        int b = 1;
+        int i = 0;
+        int c;
+        return climb(n,a,b,c,i);
     }
 };
