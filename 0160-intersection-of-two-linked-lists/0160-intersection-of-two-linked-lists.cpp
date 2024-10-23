1f6341e1c115a88200639c348a0f476e4708b7ac
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-10-23 21:28:24 +0530

    Time: 37 ms (61.11%), Space: 17.2 MB (85.94%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -12,54 +12,48 @@ public:
         ListNode* a = headA;
         ListNode* b = headB;
 
-
         while(a->next&&b->next){
-            if(a==b){
-                return a;
-            }
             a = a->next;
             b = b->next;
-
         }
 
+
         if(a->next==0){
-            // B is bigger
+            // B is greater
+            // length of b
             int Blen = 0;
-            // we got the length of B which is how much big from the start
+            // original length
             while(b->next){
                 Blen++;
                 b = b->next;
             }
-            // now place the B at the good point
+
             while(Blen--){
                 headB = headB->next;
             }
 
+
         }
         else{
-            // A is bigger
             int Alen = 0;
-            // we got the length of A which is how much big from the start
+            // original length
             while(a->next){
                 Alen++;
                 a = a->next;
             }
-            // now place the a at the good position
+
             while(Alen--){
                 headA = headA->next;
             }
         }
-        
+
 
         while(headA!=headB){
             headA = headA->next;
             headB = headB->next;
         }
 
-
         return headA;
-
-
 
     }
 };
