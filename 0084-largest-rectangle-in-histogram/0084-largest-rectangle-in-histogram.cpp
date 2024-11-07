56eee5a4af42bdbb58fc5270df194b4f6746dbb1
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-11-07 21:05:38 +0530

    Time: 50 ms (28.03%), Space: 84.5 MB (33.11%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-    vector<int>nextSmallerElement(vector<int>&arr){
+    vector<int>nextSmaller(vector<int>& arr){
         vector<int>ans(arr.size());
+
         stack<int>st;
         st.push(-1);
+
 
         for(int i=arr.size()-1;i>=0;i--){
             int curr = arr[i];
@@ -18,12 +20,15 @@ public:
 
         return ans;
     }
-    vector<int>prevSmallerElement(vector<int>&arr){
+
+    vector<int>prevSmaller(vector<int>& arr){
         vector<int>ans(arr.size());
+
         stack<int>st;
         st.push(-1);
 
-        for(int i=0;i<=arr.size()-1;i++){
+
+        for(int i=0;i<arr.size();i++){
             int curr = arr[i];
 
             while(st.top()!=-1 && arr[st.top()]>=curr){
@@ -33,24 +38,26 @@ public:
             ans[i] = st.top();
             st.push(i);
         }
-
+        
         return ans;
     }
+
     int largestRectangleArea(vector<int>& heights) {
-        vector<int>next = nextSmallerElement(heights);
-        // make sure krna ki next me koi bhi answer -1 na ho 
+        vector<int>next = nextSmaller(heights);
         for(int i=0;i<next.size();i++){
             if(next[i]==-1){
                 next[i] = next.size();
             }
         }
-        vector<int>prev = prevSmallerElement(heights);
+        vector<int>prev = prevSmaller(heights);
 
         int maxArea = INT_MIN;
 
         for(int i=0;i<heights.size();i++){
             int length = heights[i];
+
             int width = next[i]-prev[i]-1;
+
             int area = length*width;
 
             maxArea = max(area,maxArea);
