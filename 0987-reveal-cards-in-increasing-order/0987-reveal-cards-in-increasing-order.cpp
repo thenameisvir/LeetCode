e5270b4e6958fdbec31d0e38ccec6e15b38ab622
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-11-19 23:31:13 +0530

    Time: 0 ms (100%), Space: 11.2 MB (23.91%) - LeetHub

--- 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
+++ 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-
-        // Step 1 : Sorting the deck so that it will be useful to reverse verify
+        // initially sort the deck to perform the reversion
         sort(deck.begin(),deck.end());
-        // initialising the queue for the reverse nature of the question
+
         queue<int>q;
-        // initialising the ans vector which we need to return
+
         vector<int>arr(deck.size());
-        // Step 2 : Push all the indices into the queue
+
+        // push the indices into the queue so that we will perform the reverse
 
         for(int i=0;i<deck.size();i++){
             q.push(i);
         }
 
-        // take the charge of for loop for the simulation and remember that the simulation question might get solved by the help of queue.
+        // now the main loop comes into the play with the logic
 
         for(int i=0;i<deck.size();i++){
-
-            // Step 1 in for loop : push the front card into the deck
+            // pushing the first element into the queue
             arr[q.front()] = deck[i];
-
             q.pop();
-
-            // Step 2 in for loop : check whether the queue is empty or not and if it's not empty then pop the second element revising the circular nature
-
+            // check if q is not empty and then pop the first and push again
             if(!q.empty()){
                 q.push(q.front());
                 q.pop();
             }
-
-
         }
 
 
         return arr;
-
-
-
     }
 };
