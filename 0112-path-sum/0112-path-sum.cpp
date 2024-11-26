d919f912649dc3e9c84665aebc305f96550d9d4c
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-11-26 22:24:57 +0530

    Time: 0 ms (100%), Space: 21.6 MB (7.93%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,12 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (root == nullptr) return false;
-        if (root->left == nullptr && root->right == nullptr) return targetSum == root->val;
+// my first case for the path sum is , if i have tackeled the null node then
+        if(root==NULL) return false;
+// my second case if for the leaf node because then i have to check the conditions
+        if(root->left==NULL && root->right==NULL) return targetSum==root->val;
 
-        bool ans1 = hasPathSum(root->left, targetSum - root->val);
-        bool ans2 = hasPathSum(root->right, targetSum - root->val);
 
-        return ans1 || ans2;
+        // now the recursion conditions
+        bool ans1 = hasPathSum(root->left,targetSum-root->val);
+        bool ans2 = hasPathSum(root->right,targetSum-root->val);
+
+
+        return ans1||ans2;
     }
 };
