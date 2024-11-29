3408ed3dc22a75b6fd49eac260918cd8901b3141
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-11-29 21:00:09 +0530

    Time: 8 ms (35.8%), Space: 27 MB (71.55%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,29 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int searchInorder(vector<int>& inorder, int size, int target) {
-        for (int i = 0; i < size; i++) {
-            if (inorder[i] == target) {
+    int search(vector<int>& inorder, int size, int num) {
+        for (int i = 0; i < inorder.size(); i++) {
+            if (inorder[i] == num) {
                 return i;
             }
         }
         return -1;
     }
 
-    TreeNode* construct(vector<int>& preorder, vector<int>& inorder, int& preIndex, int inStart, int inEnd, int size) {
-        // Base case
-        if (preIndex >= size || inStart > inEnd) return nullptr;
+    TreeNode* constructfromPreandIn(vector<int>& preorder, vector<int>& inorder, int& preIndex, int inStart, int inEnd, int size) {
+        // Base Case
+        if (preIndex >= size || inStart > inEnd) {
+            return NULL;
+        }
 
-        // Solve 1 case
-        int e = preorder[preIndex];
+        // Convert the current preorder element to a node
+        int element = preorder[preIndex];
         preIndex++;
-        TreeNode* root = new TreeNode(e);
 
-        // Find the element in inorder
-        int position = searchInorder(inorder, size, e);
+        TreeNode* root = new TreeNode(element);
 
-        // Recursion for left and right subtrees
-        root->left = construct(preorder, inorder, preIndex, inStart, position - 1, size);
-        root->right = construct(preorder, inorder, preIndex, position + 1, inEnd, size);
+        // Search for the element in the inorder array
+        int position = search(inorder, size, element);
+
+        // Recursive calls for left and right subtrees
+        root->left = constructfromPreandIn(preorder, inorder, preIndex, inStart, position - 1, size);
+        root->right = constructfromPreandIn(preorder, inorder, preIndex, position + 1, inEnd, size);
 
         return root;
     }
@@ -34,7 +48,6 @@ public:
         int inStart = 0;
         int inEnd = size - 1;
 
-        // Start building the tree
-        return construct(preorder, inorder, preIndex, inStart, inEnd, size);
+        return constructfromPreandIn(preorder, inorder, preIndex, inStart, inEnd, size);
     }
 };
