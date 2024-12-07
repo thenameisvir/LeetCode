bb8f63f20d914dfb136da1d751d1e90196a14dd4
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-12-07 21:14:37 +0530

    Time: 0 ms (100%), Space: 14.9 MB (11.67%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,35 +12,45 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        // Initialise the vector
-        vector<vector<int>>v; // push zig-zag ans
+        // Base cases are more important than the logic haha
+        vector<vector<int>>v;
         if(!root) return v;
-
-        bool LtoR = true;
+        // Initial condition to give the stage to the logic
         queue<TreeNode*>q;
         q.push(root);
-
+        bool LtoR = true;
         while(!q.empty()){
+
             int width = q.size();
-            vector<int>level(width);
+            vector<int>v2(width);
+
+            // now traverse on the queue to fill the 1D vector
             for(int i=0;i<width;i++){
+                // Take the front node and do operations accordingly
                 TreeNode* front = q.front();q.pop();
-                
+                // we need to specify the index accordingly
                 int index = LtoR ? i : width - i - 1;
-                level[index] = front->val;
+                // state the value into the vector
+                v2[index] = front->val;
+
+                // now to maintain the queue , push all the children into it
 
                 if(front->left) q.push(front->left);
                 if(front->right) q.push(front->right);
+
+
             }
 
-            // Toggle the direction
+
+            // Now you need to toggle the boolean
             LtoR = !LtoR;
-            v.push_back(level);
+
+            v.push_back(v2);
+
         }
 
 
         return v;
-
 
     }
 };
