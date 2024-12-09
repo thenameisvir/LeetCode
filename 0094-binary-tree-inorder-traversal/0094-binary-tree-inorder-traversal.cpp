3a69e5d3455a06dbc3f1593b3b8147dd105e3fb4
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-12-09 23:08:01 +0530

    Time: 0 ms (100%), Space: 10.8 MB (26.44%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,17 +11,17 @@ public:
             } else {
                 TreeNode* pred = curr->left;
 
-                // Find the rightmost node in the left subtree or stop if it already points to curr
+                
                 while (pred->right && pred->right != curr) {
                     pred = pred->right;
                 }
 
                 if (pred->right == NULL) {
-                    // Establish the thread and move to the left child
+                    
                     pred->right = curr;
                     curr = curr->left;
                 } else {
-                    // Remove the thread, process the current node, and move to the right child
+                    
                     pred->right = NULL;
                     ans.push_back(curr->val);
                     curr = curr->right;
