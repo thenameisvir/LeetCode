037921dffe9f28c086ada34ad7be71c641a5c660
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-13 21:10:44 +0530

    Time: 302 ms (33.58%), Space: 108.9 MB (26.08%) - LeetHub

--- 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/2094-remove-stones-to-minimize-the-total.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& arr, int k) {
-        priority_queue<int> pq;
-        
-        // Push all elements into the max heap
-        for (int i = 0; i < arr.size(); i++) {
+        //Make a max heap
+        priority_queue<int>pq;
+        // pushing every element into the max heap
+        for(int i=0;i<arr.size();i++){
             pq.push(arr[i]);
         }
-
-        while (k > 0 && !pq.empty()) {
-            int element = pq.top(); // Get the largest element
-            pq.pop();
-            
-            // Remove half of the stones from the largest pile
-            element -= element / 2; 
-            
-            // Push the updated pile back into the heap
-            pq.push(element);
+        // now make it removed from the min heap;
+        while(k){
+            int front = pq.top();pq.pop();
+            front = (front) - (front/2);
+            pq.push(front);
             k--;
         }
 
-        int ans = 0;
-
-        // Sum up all remaining stones in the heap
-        while (!pq.empty()) {
-            ans += pq.top();
+        int count = 0;
+        while(!pq.empty()){
+            int e = pq.top();
+            count+=e;
             pq.pop();
         }
 
-        return ans;
+        return count;
+
+        
     }
 };
