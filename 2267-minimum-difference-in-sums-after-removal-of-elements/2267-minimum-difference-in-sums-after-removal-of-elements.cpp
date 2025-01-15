36be6a0413cd4eb9b7b309595f706732361c04b2
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-15 22:17:41 +0530

    Time: 266 ms (27.05%), Space: 163.4 MB (20.49%) - LeetHub

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+#define ll long long 
+    long long minimumDifference(vector<int>& nums) {
+        int n = nums.size()/3;
+        vector<ll>prefix(nums.size(),-1),suffix(nums.size(),-1);
+        // prefix[i] = sum of n elements from left side
+        // suffix[i] = sum of n elements from right side
+
+        ll sum = 0; // min n elements sum
+        priority_queue<ll>pq; // max heap
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            pq.push(nums[i]);
+
+            // pop out max elements
+            if(pq.size()>n){
+                sum-=pq.top();
+                pq.pop();
+
+            }
+
+            if(pq.size()==n){
+                prefix[i] = sum;
+            }
+        }
+
+
+        sum = 0; // maximum n elements sum
+        priority_queue<ll,vector<ll>,greater<ll>>pq2; // min heap
+        for(int i=nums.size()-1;i>=0;i--){
+            sum+=nums[i];
+            pq2.push(nums[i]);
+
+            // pop out max elements
+            if(pq2.size()>n){
+                sum-=pq2.top();
+                pq2.pop();
+
+            }
+
+            if(pq2.size()==n){
+                suffix[i] = sum;
+            }
+        }
+
+        ll ans = LONG_LONG_MAX;
+        for(int i=n-1;i<2*n;i++){
+            ans = min(ans,prefix[i]-suffix[i+1]);
+        }
+
+        return ans;
+
+
+
+
+
+    }
+};
