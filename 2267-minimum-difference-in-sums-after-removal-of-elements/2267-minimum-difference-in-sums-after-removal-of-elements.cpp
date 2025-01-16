407e058f0cf6062f918d4b0033221da39dd4596d
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-16 09:44:09 +0530

    Time: 261 ms (33.6%), Space: 163.4 MB (20.24%) - LeetHub

--- 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -3,11 +3,11 @@ public:
 #define ll long long
     long long minimumDifference(vector<int>& nums) {
         int n = nums.size()/3;
-        // make vectors for prefix and suffix
+
         vector<ll>prefix(nums.size(),-1),suffix(nums.size(),-1);
 
-        ll sum = 0;
         priority_queue<ll>pq;
+        ll sum = 0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             pq.push(nums[i]);
@@ -16,15 +16,14 @@ public:
                 sum-=pq.top();
                 pq.pop();
             }
-
             if(pq.size()==n){
                 prefix[i] = sum;
             }
         }
 
-        sum = 0;
         priority_queue<ll,vector<ll>,greater<ll>>pq2;
-        
+        sum = 0;
+
         for(int i=nums.size()-1;i>=0;i--){
             sum+=nums[i];
             pq2.push(nums[i]);
@@ -33,18 +32,18 @@ public:
                 sum-=pq2.top();
                 pq2.pop();
             }
-
             if(pq2.size()==n){
                 suffix[i] = sum;
             }
         }
 
-        ll ans = LONG_LONG_MAX;
 
+        ll ans = LONG_LONG_MAX;
         for(int i=n-1;i<2*n;i++){
             ans = min(ans,prefix[i]-suffix[i+1]);
         }
 
         return ans;
+        
     }
 };
