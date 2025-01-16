0b4507e2d2743d286050f710e14623b991d72345
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-16 23:51:39 +0530

    Time: 3 ms (69.59%), Space: 65 MB (82.56%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            // first take out the index
+            // sabse pehle index nikalke lao abs karke dhyan rkhna bhulna mt
             int index = abs(nums[i]);
+            
+            // ab check karo jaha aaye ho vo negative toh nahi hai
             if(nums[index]<0) return index;
+
+            // agar vo negative nahi tha aur return nhi ho paya toh usko minus kardo
 
             nums[index]*=-1;
         }
 
         return -1;
+
     }
 };
