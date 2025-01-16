eb2efeeed9954ccd08cd55529792b63f27f9e101
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-16 22:45:50 +0530

    Time: 15 ms (91.65%), Space: 139.2 MB (56.7%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        deque<int>q;
-        vector<int>v;
+         deque<int>q;
+         vector<int>v;
 
-        // Vir first window ko access kar
-        for(int i=0;i<k;i++){
+         // first iterate first window
+         for(int i=0;i<k;i++){
             while(!q.empty() && arr[q.back()]<=arr[i]){
                 q.pop_back();
             }
 
             q.push_back(i);
-        }
+         }
 
-        // yaha tak apan ne window bana li hai almost ab iske front p ans pada hai pehli window ka
-        // usko bhi lage hat push karde
+         v.push_back(arr[q.front()]);
 
-        v.push_back(arr[q.front()]);
-
-        // now we will be accessing the rest of the window 
-        for(int i=k;i<arr.size();i++){
-            // check kar jo max element hum push kr rhe the kahi vo out of bound to nahi ho gya
+         for(int i=k;i<arr.size();i++){
             if(!q.empty() && q.front()<=i-k){
                 q.pop_front();
             }
@@ -33,8 +28,9 @@ public:
 
             v.push_back(arr[q.front()]);
 
-        }
 
-        return v;
+         }
+
+         return v;
     }
 };
