e25d9d07fd85a8ace5db45c669d2be701094b122
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-21 22:15:22 +0530

    Time: 0 ms (100%), Space: 8.4 MB (9.45%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
+    int solveUsingMem(int n,vector<int>&dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        // just base case k baad step no 3
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n] = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
+
+        return dp[n];
+    }
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
+        // create a dp array and do understand that you are doing solution upto n
+        // so make upto it only 
 
-        int fibo = fib(n-1) + fib(n-2);
-
-        return fibo;
+        vector<int>dp(n+1,-1);
+        int ans = solveUsingMem(n,dp);
+        return ans;
+        
     }
 };
