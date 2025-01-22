8e7217389ab48ca931513fd6f9651a9587a50e4c
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-23 00:24:45 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,19 +3,24 @@ public:
     int solveUsingTabulation(vector<int>& nums) {
         int n = nums.size();
         //step1
-        vector<int> dp(n, -1);
+        // vector<int> dp(n, -1);
         //step2
-        dp[n-1] = nums[n-1];
+        int prev = nums[n-1];
+        int next = 0;
+        int curr;
         for(int index = n-2; index>=0; index--) {
             int tempAns = 0;
             if(index + 2 < n) {
-                tempAns = dp[index+2];
+                tempAns = next;
             }
             int include = nums[index] + tempAns;
-            int exclude = 0 + dp[index+1];
-            dp[index] = max(include, exclude);
+            int exclude = 0 + prev;
+            curr = max(include, exclude);
+
+            next = prev;
+            prev = curr;
         }
-        return dp[0];
+        return prev;
     }
     int rob(vector<int>& nums) {
         int ans = solveUsingTabulation(nums);
