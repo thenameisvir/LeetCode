2929b3d44751fe06e40e4ae9e67f93e30363bbb8
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-22 23:10:26 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    int usingRec(vector<int>& arr,int n){
-        if(n>=arr.size()){
-            return 0;
-        }
-        // base case was easy let;s do the processing
-        int inc = arr[n] + usingRec(arr,n+2);
-        int exc = 0 + usingRec(arr,n+1);
-
-        // this is based on include exclude principle
-
-        // now here we return the max value which will go upon the recursive tree
-
-        return max(inc,exc);
-
-
-    }
-    int usingDp(vector<int>& arr,int n,vector<int>&dp){
-        if(n>=arr.size()){
+    int solveUsingMemo(vector<int>& arr,int index,vector<int>&dp){
+        if(index>=arr.size()){
             return 0;
         }
 
-        // idhar dp me store karenge
-        if(dp[n]!=-1){
-            return dp[n];
+        // dp ko process karlo na 
+
+        if(dp[index]!=-1){
+            return dp[index];
         }
 
+        // ek case solve karo include exclude ka 
+        int inc = arr[index] + solveUsingMemo(arr,index+2,dp);
+        int exc = 0 + solveUsingMemo(arr,index+1,dp);
 
-        // idhar khudka process karo
-        int inc = arr[n] + usingDp(arr,n+2,dp);
-        int exc = 0 + usingDp(arr,n+1,dp);
+        dp[index] = max(inc,exc);
 
-        dp[n] = max(inc,exc);
-        return dp[n];
+        return dp[index];
     }
     int rob(vector<int>& arr) {
         vector<int>dp(arr.size(),-1);
-        int ans = usingDp(arr,0,dp);
-        return ans;
-        
+        int ans2 = solveUsingMemo(arr,0,dp);
+        return ans2;
     }
 };
