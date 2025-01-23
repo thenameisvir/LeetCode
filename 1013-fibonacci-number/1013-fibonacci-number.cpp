1cd469cc3cbc0cca89cc25b55f6651deee226036
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-23 21:04:28 +0530

    Time: 10 ms (48.37%), Space: 8.2 MB (9.63%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    int solveUsingMem(int n,vector<int>&dp){
+    int usingRec(int n){
+        // base case baad me likhenge 
         if(n==0 || n==1){
             return n;
         }
-        // just base case k baad step no 3
+
+        // ek case tum sambhalo baki recursion dekh lega 
+        int ans = usingRec(n-1) + usingRec(n-2);
+
+        return ans;
+
+    }
+    int usingMemoization(int n,vector<int>&dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        // base case remains the same
+
+
+        // ek case tum kardo madhur bhai baki mai hu na store kr lunga 
         if(dp[n]!=-1){
             return dp[n];
         }
 
-        dp[n] = solveUsingMem(n-1,dp) + solveUsingMem(n-2,dp);
+        int ans = usingMemoization(n-1,dp) + usingMemoization(n-2,dp);
 
+        dp[n] = ans;
         return dp[n];
+
+    }
+    int usingSpaceOP(int n){
+        // behencho isme vector lenge hi kyu ye bata 
+        return -1;
+        
     }
     int fib(int n) {
-        // create a dp array and do understand that you are doing solution upto n
-        // so make upto it only 
-
         vector<int>dp(n+1,-1);
-        int ans = solveUsingMem(n,dp);
-        return ans;
-        
+        int ans1 = usingRec(n);
+        int ans2 = usingMemoization(n,dp);
+        return ans2;
     }
 };
