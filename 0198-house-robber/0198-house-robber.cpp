a27034120d71869ece531d83c9acba7d7693bbe2
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-23 23:25:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,14 +15,29 @@ public:
         // ek case solve karde
         int inc = nums[index] + solveUsingMemo(nums,index+2,dp);
         int exc = 0 + solveUsingMemo(nums,index+1,dp);
-
+        // return max(inc,exc);
         int ans = max(inc,exc);
         dp[index] = ans;
         return dp[index];
     }
+    int solveByTabulation(vector<int>& nums){
+        vector<int>dp(nums.size()+1,0);
+        int n = nums.size();
+        // ek ans dalna padega humko somehow
+        dp[n-1] = nums[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+        int inc = nums[i] + dp[i+2];
+        int exc = 0 + dp[i+1];
+
+        dp[i] = max(inc,exc);
+        }
+
+        return dp[0];
+    }
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size(),-1);
-        int ans = solveUsingMemo(nums,0,dp);
+        int ans = solveByTabulation(nums);
         return ans;
     }
 };
