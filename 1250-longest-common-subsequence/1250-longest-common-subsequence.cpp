0f1713a3268318d1f674871d53f6b3a3172b18c6
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-29 09:17:49 +0530

    Time: 46 ms (14.36%), Space: 27.7 MB (6.39%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,59 +1,50 @@
 class Solution {
 public:
-    int usingRec(string text1, string text2,int i,int j){
-        // base case toh lund smjh na aaye 
-        if(i>=text1.length()){
-            return 0;
-        }
-        if(j>=text2.length()){
-            return 0;
-        }
+    int usingRec(string &a, string &b,int i,int j){
+        // base case will be written after one processing
+        if(i>=a.length()) return 0;
+        if(j>=b.length()) return 0;
 
+
+
+        // one case to be solved
         int ans = 0;
-        if(text1[i]==text2[j]){
-            ans = 1+ usingRec(text1,text2,i+1,j+1); // ye sb baad me dekhenge
+        // if my both of the char are matching
+        if(a[i]==b[j]){
+            ans = 1 + usingRec(a,b,i+1,j+1);
         }
         else{
-            ans = max(usingRec(text1,text2,i+1,j),usingRec(text1,text2,i,j+1));
+            ans = max(usingRec(a,b,i+1,j),usingRec(a,b,i,j+1));
         }
 
         return ans;
-
     }
-    int usingMemo(string &text1, string &text2,int i,int j,vector<vector<int>>&dp){
-        if(i>=text1.length()){
-            return 0;
-        }
-        if(j>=text2.length()){
-            return 0;
-        }
+    int usingDP(string &a, string &b,int i,int j,vector<vector<int>>&dp){
+        if(i>=a.length()) return 0;
+        if(j>=b.length()) return 0;
+
 
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
 
+        // one case to be solved
         int ans = 0;
-        if(text1[i]==text2[j]){
-            ans = 1+ usingMemo(text1,text2,i+1,j+1,dp); // ye sb baad me dekhenge
+        // if my both of the char are matching
+        if(a[i]==b[j]){
+            ans = 1 + usingDP(a,b,i+1,j+1,dp);
         }
         else{
-            ans = max(usingMemo(text1,text2,i+1,j,dp),usingMemo(text1,text2,i,j+1,dp));
+            ans = max(usingDP(a,b,i+1,j,dp),usingDP(a,b,i,j+1,dp));
         }
 
         dp[i][j] = ans;
-
         return dp[i][j];
-
-
-
     }
-    int longestCommonSubsequence(string text1, string text2) {
+    int longestCommonSubsequence(string a, string b) {
         int i = 0;
         int j = 0;
-    vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size(),-1));
-        int ans = usingMemo(text1,text2,i,j,dp);
-        return ans;
-
-
+        vector<vector<int>>dp(a.length()+1,vector<int>(b.length(),-1));
+        int ans = usingDP(a,b,i,j,dp); return ans;
     }
 };
