a1289febeed6435d5f21efa469ac20ecae87e672
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-29 10:04:23 +0530

    Time: 34 ms (32.17%), Space: 45.3 MB (6.39%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -41,10 +41,35 @@ public:
         dp[i][j] = ans;
         return dp[i][j];
     }
+    int usingTab(string a, string b){
+    vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
+    // fill something into the dp according to the base cases 
+    // here we took the size of the 2D matrix increasd because we need to attain indexes in tabulation
+    // so to not get out of bound this is mandatory
+    for(int i=a.length()-1;i>=0;i--){
+        for(int j=b.length()-1;j>=0;j--){
+        int ans = 0;
+        // if my both of the char are matching
+        if(a[i]==b[j]){
+            ans = 1 + dp[i+1][j+1];
+        }
+        else{
+            ans = 0 + max(dp[i+1][j],dp[i][j+1]);
+        }
+        dp[i][j] = ans;
+        }
+
+
+    }
+
+    return dp[0][0];
+
+
+    }
     int longestCommonSubsequence(string a, string b) {
         int i = 0;
         int j = 0;
         vector<vector<int>>dp(a.length()+1,vector<int>(b.length(),-1));
-        int ans = usingDP(a,b,i,j,dp); return ans;
+        int ans = usingTab(a,b); return ans;
     }
 };
