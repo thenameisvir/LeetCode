c1e7359303806bd71e922feedebe4082ddf413fa
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-01-30 16:08:58 +0530

    Time: 63 ms (13.64%), Space: 83.3 MB (5.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int usingRec(string &word1, string &word2,int i,int j){
+    int usingMemo(string word1, string word2,int i,int j,vector<vector<int>>&dp){
+        // base case would be the same as the recursion
         if(i>=word1.length()){
             return word2.length()-j;
         }
@@ -8,60 +9,32 @@ public:
             return word1.length()-i;
         }
 
-        // base case has been got up to tight
 
-        int ans = 0;
-        if(word1[i]==word2[j]){
-            ans = 0 + usingRec(word1,word2,i+1,j+1);
-        }
-        else{
-            int replace = 1 + usingRec(word1,word2,i+1,j+1);
-            int insert = 1 + usingRec(word1,word2,i,j+1);
-            int remove = 1 + usingRec(word1,word2,i+1,j);
-
-            ans = min(replace,min(insert,remove));
-        }
-
-        
-
-        return ans;
-
-    }
-    int usingMemo(string &word1, string &word2,int i,int j,vector<vector<int>>&dp){
-         if(i>=word1.length()){
-            return word2.length()-j;
-        }
-        if(j>=word2.length()){
-            return word1.length()-i;
-        } // base case would remain same throughout the logic ... 
-
-        // store in the dp array
+        // store into the dp array
         if(dp[i][j]!=0){
             return dp[i][j];
         }
 
-        // copy paste the whole logic
         int ans = 0;
+
         if(word1[i]==word2[j]){
             ans = 0 + usingMemo(word1,word2,i+1,j+1,dp);
         }
         else{
             int replace = 1 + usingMemo(word1,word2,i+1,j+1,dp);
-            int insert = 1 + usingMemo(word1,word2,i,j+1,dp);
-            int remove = 1 + usingMemo(word1,word2,i+1,j,dp);
+            int insert = 1 + usingMemo(word1,word2,i+1,j,dp);
+            int remove = 1 + usingMemo(word1,word2,i,j+1,dp);
 
             ans = min(replace,min(insert,remove));
         }
 
         dp[i][j] = ans;
         return dp[i][j];
-
     }
+
     int minDistance(string word1, string word2) {
-        int i = 0;
-        int j = 0;
-        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1.0));
-        int ans = usingMemo(word1,word2,i,j,dp);
+        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,0));
+        int ans = usingMemo(word1,word2,0,0,dp);
         return ans;
     }
 };
