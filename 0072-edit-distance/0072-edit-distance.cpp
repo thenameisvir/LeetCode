00fe8a7daab7e50259447066503b4baf9afa3753
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-02-01 09:25:06 +0530

    Time: 67 ms (10.11%), Space: 83.3 MB (5.3%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int usingMemo(string word1, string word2,int i,int j,vector<vector<int>>&dp){
-        // base case would be the same as the recursion
+    int Memo(string word1, string word2,int i,int j,vector<vector<int>>&dp){
+        // base case baad me dekhenge 
         if(i>=word1.length()){
             return word2.length()-j;
         }
         if(j>=word2.length()){
             return word1.length()-i;
         }
-
-
-        // store into the dp array
+        // check if it is present in the dp array
         if(dp[i][j]!=0){
             return dp[i][j];
         }
 
+        // now put the recursion
         int ans = 0;
-
         if(word1[i]==word2[j]){
-            ans = 0 + usingMemo(word1,word2,i+1,j+1,dp);
+            ans = 0 + Memo(word1,word2,i+1,j+1,dp);
         }
         else{
-            int replace = 1 + usingMemo(word1,word2,i+1,j+1,dp);
-            int insert = 1 + usingMemo(word1,word2,i+1,j,dp);
-            int remove = 1 + usingMemo(word1,word2,i,j+1,dp);
+            // now in the edit game we will do 3 cases to make it correct 
+            int replace = 1 + Memo(word1,word2,i+1,j+1,dp);
+            // here in replace we did i+1,j+1 becaue we are almost making it 
+            int insert = 1 + Memo(word1,word2,i+1,j,dp);
+            // here we inserted to the word1 and making it matchable
+            int remove = 1 + Memo(word1,word2,i,j+1,dp);
 
             ans = min(replace,min(insert,remove));
         }
@@ -31,10 +32,9 @@ public:
         dp[i][j] = ans;
         return dp[i][j];
     }
-
     int minDistance(string word1, string word2) {
         vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,0));
-        int ans = usingMemo(word1,word2,0,0,dp);
+        int ans = Memo(word1,word2,0,0,dp);
         return ans;
     }
 };
