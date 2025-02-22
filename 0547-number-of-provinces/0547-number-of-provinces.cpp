c0de288fbe0f61a17b844b7463cfd1ff25342316
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-02-23 01:32:52 +0530

    Time: 3 ms (43.52%), Space: 19.7 MB (56.27%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    void dfs(int src,unordered_map<int,bool>&vis,vector<vector<int>>& arr,int n,unordered_map<int, list<int>> &adj){
+    void dfs(int src,unordered_map<int,bool>&vis,vector<vector<int>>& isConnected,int n){
         vis[src] = true;
 
-        for(auto i:adj[src]){
-            if(!vis[i]){
-                dfs(i,vis,arr,n,adj);
+        // yaha adjacncy list nahi hai col me krna padega
+        int col = n;
+        int row = src;
+
+        for(int i = 0;i<col;i++){
+            if(src!=i && isConnected[row][i]==1 && !vis[i]){
+                dfs(i,vis,isConnected,n);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& arr) {
-        // adjacency list banate hai bc 
-        int n = arr.size();
-        unordered_map<int, list<int>> adj;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+         int n = isConnected.size();
+         unordered_map<int,bool>vis;
+         int count = 0;
+         for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,isConnected,n);
+                count++;
+            }
+         }
 
-for (int i = 0; i < arr.size(); i++) {
-    for (int j = 0; j < arr[i].size(); j++) {
-        if (arr[i][j] == 1 && i != j) {
-            adj[i].push_back(j);
-            adj[j].push_back(i);
-        }
-    }
-}
-    int count = 0;
-    unordered_map<int,bool>vis;
-    for(int i=0;i<arr.size();i++){
-        if(!vis[i]){
-            dfs(i,vis,arr,n,adj);
-        count++;
-    }
-
-    }
-
-    return count;
-
+         return count;
     }
 };
