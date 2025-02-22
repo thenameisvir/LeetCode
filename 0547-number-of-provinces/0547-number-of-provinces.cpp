51efa763c50ab74a4c901c1155ac39ce716f26df
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-02-23 01:09:25 +0530

    Time: 3 ms (43.52%), Space: 19.7 MB (56.27%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void dfs(int src,unordered_map<int,bool>&vis,vector<vector<int>>& isConnected,int n){
+        vis[src] = true;
+
+        // yaha adjacncy list nahi hai col me krna padega
+        int col = n;
+        int row = src;
+
+        for(int i = 0;i<col;i++){
+            if(src!=i && isConnected[row][i]==1 && !vis[i]){
+                dfs(i,vis,isConnected,n);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+         int n = isConnected.size();
+         unordered_map<int,bool>vis;
+         int count = 0;
+         for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,isConnected,n);
+                count++;
+            }
+         }
+
+         return count;
+    }
+};
