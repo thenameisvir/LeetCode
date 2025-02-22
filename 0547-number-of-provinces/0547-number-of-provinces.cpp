9602ca9256fdc483511472817e15320e5bdcd56f
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-02-23 01:15:56 +0530

    Time: 18 ms (6.07%), Space: 24.3 MB (5.26%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    void dfs(int src,unordered_map<int,bool>&vis,vector<vector<int>>& isConnected,int n){
+    void dfs(int src,unordered_map<int,bool>&vis,vector<vector<int>>& arr,int n,unordered_map<int, list<int>> &adj){
         vis[src] = true;
 
-        // yaha adjacncy list nahi hai col me krna padega
-        int col = n;
-        int row = src;
-
-        for(int i = 0;i<col;i++){
-            if(src!=i && isConnected[row][i]==1 && !vis[i]){
-                dfs(i,vis,isConnected,n);
+        for(auto i:adj[src]){
+            if(!vis[i]){
+                dfs(i,vis,arr,n,adj);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-         int n = isConnected.size();
-         unordered_map<int,bool>vis;
-         int count = 0;
-         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,isConnected,n);
-                count++;
-            }
-         }
+    int findCircleNum(vector<vector<int>>& arr) {
+        // adjacency list banate hai bc 
+        int n = arr.size();
+        unordered_map<int, list<int>> adj;
 
-         return count;
+for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[i].size(); j++) {
+        if (arr[i][j] == 1 && i != j) {
+            adj[i].push_back(j);
+            adj[j].push_back(i);
+        }
+    }
+}
+    int count = 0;
+    unordered_map<int,bool>vis;
+    for(int i=0;i<arr.size();i++){
+        if(!vis[i]){
+            dfs(i,vis,arr,n,adj);
+        count++;
+    }
+
+    }
+
+    return count;
+
     }
 };
