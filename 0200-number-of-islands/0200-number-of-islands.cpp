6d1e2fe673d2553afb8281c5ea978d93297f28cc
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-02-25 00:30:25 +0530

    Time: 117 ms (5.05%), Space: 35.5 MB (5.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,60 +1,59 @@
-class Solution
-{
+class Solution {
 public:
-    bool isSafe(int i, int j, vector<vector<char>> &grid, map<pair<int, int>, bool> &vis)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        if (i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1' && !vis[{i, j}])
+    bool isSafe(int x,int y,map<pair<int,int>,bool>&vis,vector<vector<char>>& arr){
+        if(x>=0 && x<arr.size() && y>=0 && y<arr[0].size() && !vis[{x,y}] && arr[x][y]=='1'){
             return true;
-        return false;
+        }
+        else{
+            return false;
+        }
     }
-
-    void bfs(int i, int j, vector<vector<char>> &grid, map<pair<int, int>, bool> &vis)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        vis[{i, j}] = true;
-        while (!q.empty())
-        {
-            auto frontNode = q.front();
+    void bfs(int srcx, int srcy,map<pair<int,int>,bool>&vis,vector<vector<char>>& arr){
+        // yaha ayega main logic ab tak apan ne original calls kar di hai
+        // yaha par mai initial state maintain krne ki sochunga
+        queue<pair<int,int>>q;
+        q.push({srcx,srcy});
+        vis[{srcx,srcy}] = true;
+        // yaha tak apan logo ne inital state maintain karli hai ab kaam shuru karte hai
+        while(!q.empty()){
+            auto frontPair = q.front();
+            int tempx = frontPair.first;
+            int tempy = frontPair.second;
             q.pop();
-            int tempX = frontNode.first;
-            int tempY = frontNode.second;
-            int dx[] = {-1, 0, 1, 0};
-            int dy[] = {0, 1, 0, -1};
-            for (int k = 0; k < 4; k++)
-            {
-                int newX = tempX + dx[k];
-                int newY = tempY + dy[k];
-                if (isSafe(newX, newY, grid, vis))
-                {
-                    q.push({newX, newY});
-                    vis[{newX, newY}] = true;
+
+            int dx[] = {-1,0,1,0};
+            int dy[] = {0,1,0,-1};
+
+            // yaha par yaad rakhne wali cheese ye hai ki loop 4 tak chalayenge kyuki 4 
+            // conditions hi toh check karni hai 
+            for(int i=0;i<4;i++){
+                int newX = tempx + dx[i];
+                int newY = tempy + dy[i];
+
+                if(isSafe(newX,newY,vis,arr)){
+                    // toh apna kaam kardo chup chaap
+                    vis[{newX,newY}] = true;
+                    q.push({newX,newY});
                 }
             }
         }
     }
-
-    int numIslands(vector<vector<char>> &grid)
-    {
-        map<pair<int, int>, bool> vis;
-        int count = 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (!vis[{i, j}] && grid[i][j] == '1')
-                {
-                    bfs(i, j, grid, vis);
+    int numIslands(vector<vector<char>>& arr) {
+        int count = 0; // yahi toh return karna hai mujhe after all
+        map<pair<int,int>,bool>vis; // isme mere indexes ayenge aur true mark karna hoga
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[i].size();j++){
+                // yaha ayega thoda bhot logic check
+                if(!vis[{i,j}] && arr[i][j]=='1'){
+                    bfs(i,j,vis,arr);
                     count++;
                 }
             }
         }
+
+
         return count;
+
+
     }
 };
