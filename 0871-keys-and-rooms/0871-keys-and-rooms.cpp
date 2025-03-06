b8459b77e79039e5db31229d6be96d2afade735a
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-06 16:15:10 +0530

    Time: 0 ms (100%), Space: 14.6 MB (59.07%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    void bfs(unordered_map<int,bool>& vis, unordered_map<int, list<int>>& adj, queue<int>& q) {
-        while (!q.empty()) {
+    bool canVisitAllRooms(vector<vector<int>>& arr) {
+        vector<bool>vis(arr.size(),false);
+        queue<int>q;
+        q.push(0);
+        vis[0] = true;
+
+        // initial processing ho chuki hai 
+        while(!q.empty()){
             int front = q.front();
             q.pop();
 
-            for (auto i : adj[front]) {
-                if (!vis[i]) {
+            for(auto i:arr[front]){
+                if(!vis[i]){
                     vis[i] = true;
                     q.push(i);
                 }
             }
         }
-    }
 
-    bool canVisitAllRooms(vector<vector<int>>& arr) {
-        unordered_map<int, list<int>> adj;
-        int n = arr.size(); // total number of rooms
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < arr[i].size(); j++) {
-                adj[i].push_back(arr[i][j]);
-                // DO NOT add reverse edge: this is a directed graph (keys only work in one direction)
-            }
+        for(auto i:vis){
+            if(!i) return false;
         }
 
-        unordered_map<int, bool> vis;
-        queue<int> q;
-        q.push(0);
-        vis[0] = true;
-
-        bfs(vis, adj, q);
-
-        // Check if all rooms are visited
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) return false;
-        }
         return true;
     }
 };
