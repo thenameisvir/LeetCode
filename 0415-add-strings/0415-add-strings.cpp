810967b85f29cd460bb36a02cee6a541f77b4ee2
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-07 15:51:37 +0530

    Time: 101 ms (5.06%), Space: 232.8 MB (5.03%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-        int i = num1.size()-1;
-        int j = num2.size()-1;
-        string str = "";
-        // carry universal rahegi aaj k baad bhulna mat
-        int carry = 0;
-        while(i>=0 || j>=0 || carry!=0){
-            int a = 0, b = 0;
-            if(i>=0){
-                a = num1[i]-'0';
-                i--;
-            }
-            if(j>=0){
-                b = num2[j]-'0';
-                j--;
-            }
+    void solve(int i,int j,string &str,int carry,string num1, string num2){
+        if(i<0 && j<0 && carry==0){
+            return;
+        }
+        int a = 0,b = 0;
 
-            // yaha tak maine elements nikal liye hai 
-            int sum = a + b + carry;
-            if(sum>=10){
-                carry = sum/10;
-            }
-            else{
-                carry = 0;
-            }
-
-            int ld = sum%10;
-            // yaha maine carry ko bhi store kara liya hai ache se aur sb set hai
-            str = char(ld+'0') + str;
+        // ek case solve kardo 
+        if(i>=0){
+            a = num1[i] -'0';
+            i--;
+        }
+        if(j>=0){
+            b = num2[j] - '0';
+            j--;
         }
 
-        if(str=="") str = "0";
+        int sum = a + b + carry;
+        if(sum>=10){
+            carry = sum/10;
+        }
+        else{
+            carry = 0;
+        }
+
+        int ld = sum%10;
+
+        str = char(ld+'0') + str;
+        solve(i,j,str,carry,num1,num2);
+
+    }
+    string addStrings(string num1, string num2) {
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        string str = "";
+        int carry = 0;
+        solve(i,j,str,carry,num1,num2);
+        if(str=="") str="0";
 
 
         return str;
