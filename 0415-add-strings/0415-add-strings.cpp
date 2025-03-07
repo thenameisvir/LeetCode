4c53bd1d57165445fbc8034da41fd4f9c5f06bde
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-07 14:48:53 +0530

    Time: 24 ms (15.56%), Space: 58.6 MB (18.18%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int i = num1.size() - 1;
-        int j = num2.size() - 1;
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        string str = "";
+        // carry universal rahegi aaj k baad bhulna mat
         int carry = 0;
-        string result = "";
-
-        while (i >= 0 || j >= 0 || carry > 0) {  
+        while(i>=0 || j>=0 || carry!=0){
             int a = 0, b = 0;
-
-            if (i >= 0) {  
-                a = num1[i] - '0';
+            if(i>=0){
+                a = num1[i]-'0';
                 i--;
-            } 
-
-            if (j >= 0) {  
-                b = num2[j] - '0';
+            }
+            if(j>=0){
+                b = num2[j]-'0';
                 j--;
             }
 
-            int sum = a + b + carry;  
-            
-            if (sum >= 10) {  
-                carry = sum / 10;  
-            } else {  
-                carry = 0;  
+            // yaha tak maine elements nikal liye hai 
+            int sum = a + b + carry;
+            if(sum>=10){
+                carry = sum/10;
+            }
+            else{
+                carry = 0;
             }
 
-            int last_digit = sum % 10;
-            result = char(last_digit + '0') + result;  
+            int ld = sum%10;
+            // yaha maine carry ko bhi store kara liya hai ache se aur sb set hai
+            str = char(ld+'0') + str;
         }
 
-        return result;
+        if(str=="") str = "0";
+
+
+        return str;
     }
 };
