8fb4545863dfa5f9a774fde5590186e73bfa32ae
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-07 14:27:06 +0530

    Time: 26 ms (12.79%), Space: 58.6 MB (19.89%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -6,16 +6,29 @@ public:
         int carry = 0;
         string result = "";
 
-        while (i >= 0 || j >= 0 || carry) {
-            int a = (i >= 0) ? num1[i] - '0' : 0;
-            int b = (j >= 0) ? num2[j] - '0' : 0;
-            int sum = a + b + carry;
-            
-            result = char((sum % 10) + '0') + result;  // Add last digit to result
-            carry = sum / 10;  // Carry forward
+        while (i >= 0 || j >= 0 || carry > 0) {  
+            int a = 0, b = 0;
 
-            i--;  // Move left
-            j--;
+            if (i >= 0) {  
+                a = num1[i] - '0';
+                i--;
+            } 
+
+            if (j >= 0) {  
+                b = num2[j] - '0';
+                j--;
+            }
+
+            int sum = a + b + carry;  
+            
+            if (sum >= 10) {  
+                carry = sum / 10;  
+            } else {  
+                carry = 0;  
+            }
+
+            int last_digit = sum % 10;
+            result = char(last_digit + '0') + result;  
         }
 
         return result;
