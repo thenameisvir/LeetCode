ad65a7a5afcbb329baaf654614bf38aab438e5a7
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-07 14:25:50 +0530

    Time: 16 ms (23.66%), Space: 58.6 MB (18.18%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    void addRe(string&num1, string&num2, int p1, int p2, int carry, string&ans){
-        // base cases
-
-        if(p1<0 && p2<0){
-            if(carry!=0){
-                ans.push_back(carry + '0');
-            }
-
-            return;
-        }
-
-        // ek case solve karo
-        int n1 = 0; int n2 = 0;
-        if(p1>=0){
-            n1 = num1[p1] - '0';
-        }
-        if(p2>=0){
-            n2 = num2[p2] - '0';
-        }
-        int csum = n1+n2+carry;
-        int digit = csum%10;
-        carry = csum/10;
-
-        ans.push_back(digit+'0');
-
-        // Recursive relation
-        addRe(num1,num2,p1-1,p2-1,carry,ans);
-
-
-    }
     string addStrings(string num1, string num2) {
-        string ans = "";
-        addRe(num1,num2,num1.size()-1,num2.size()-1,0,ans);
-        reverse(ans.begin(),ans.end());
-        return ans;
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string result = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+            int a = (i >= 0) ? num1[i] - '0' : 0;
+            int b = (j >= 0) ? num2[j] - '0' : 0;
+            int sum = a + b + carry;
+            
+            result = char((sum % 10) + '0') + result;  // Add last digit to result
+            carry = sum / 10;  // Carry forward
+
+            i--;  // Move left
+            j--;
+        }
+
+        return result;
     }
 };
