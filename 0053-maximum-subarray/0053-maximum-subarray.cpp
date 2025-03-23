8b767e2fbe2a7f86d2f9bf1bcaf452b9edbade00
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-23 12:03:28 +0530

    Time: 0 ms (100%), Space: 71.6 MB (97.78%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
-    int answer(vector<int>& arr,int s,int e){
-    
-    if(s==e) return arr[s];
-    int maxLeftBorderSum = INT_MIN, maxRightBorderSum = INT_MIN;
-
-    int m = s+(e-s)/2;
-
-    int maxLeftSum = answer(arr,s,m);
-    int maxRightSum = answer(arr,m+1,e);
-
-    int leftBorderSum = 0, rightBorderSum = 0;
-    for(int i=m;i>=s;i--){
-        leftBorderSum+=arr[i];
-        if(leftBorderSum>maxLeftBorderSum){
-            maxLeftBorderSum = leftBorderSum;
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0], currSum = 0;
+        for (int num : nums) {
+            currSum = max(num, currSum + num);
+            maxSum = max(maxSum, currSum);
         }
-    }
-
-    for(int i=m+1;i<=e;i++){
-        rightBorderSum+=arr[i];
-        if(rightBorderSum>maxRightBorderSum){
-            maxRightBorderSum = rightBorderSum;
-        }
-    }
-
-    int crossBorderSum = maxRightBorderSum + maxLeftBorderSum;
-
-    return max(crossBorderSum,max(maxLeftSum,maxRightSum));
-
-
-
-    }
-    int maxSubArray(vector<int>& arr) {
-        return answer(arr,0,arr.size()-1);
+        return maxSum;
     }
 };
