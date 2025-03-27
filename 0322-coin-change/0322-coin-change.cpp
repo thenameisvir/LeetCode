eec9597389ea8e039bd51b04d88b5d7b51aedcff
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-27 21:26:12 +0530

    Time: 39 ms (44.09%), Space: 19.2 MB (48.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    int usingMemo(vector<int>&coins,int amount,vector<int>&dp){
-        if(amount==0){
-            return 0;
-        }
-        // second base case sochenge abhi
-        if(amount<0){
-            return INT_MAX;
-        }
-        // change kya ho rha hai amount change ho raha hai
-        if(dp[amount]!=-1){
-            return dp[amount];
-        }
+    int solve(vector<int>& coins, int amount,vector<int>&dp){
+        if(amount==0) return 0;
+        if(amount<0) return INT_MAX;
+        // base case ho gaye hai ab main case
+
+        if(dp[amount]!=-1) return dp[amount];
         int mini = INT_MAX;
-         for(int i=0;i<coins.size();i++){
-            // yaha ayega main logic
-            
+        for(int i=0;i<coins.size();i++){
             int recAns;
-            if(amount<coins[i]){
+
+            if(coins[i]>amount){
                 recAns = INT_MAX;
             }
 
-            recAns = usingMemo(coins,amount-coins[i],dp);
-            
+            recAns = solve(coins,amount-coins[i],dp);
             if(recAns!=INT_MAX){
-                int ans = 1 + recAns;
-                mini = min(ans,mini);
+                int temp = 1 + recAns;
+                mini = min(temp,mini);
             }
+
+            
         }
-
         dp[amount] = mini;
+
         return dp[amount];
-
-
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,-1);
-        int ans = usingMemo(coins,amount,dp);
+        int ans = solve(coins,amount,dp);
         if(ans==INT_MAX) return -1;
         return ans;
     }
