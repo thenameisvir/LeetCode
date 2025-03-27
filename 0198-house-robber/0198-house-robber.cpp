e684b10059be8aff4daa4f4864e1124ae8ccc913
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-03-27 16:45:17 +0530

    Time: 3 ms (5.91%), Space: 10.7 MB (21.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,23 +15,18 @@ public:
     }
     int tabulation(vector<int>& nums){
         int n = nums.size();
-        vector<int>dp(n,0);
+        vector<int>dp(n+1,0);
 
         dp[n-1] = nums[n-1];
         for(int i=n-2;i>=0;i--){
         int tempAns = 0;
-        if(i+2<n){
-            tempAns = dp[i+2];
-        }
-        int inc = nums[i] + tempAns;
+        
+        int inc = nums[i] + dp[i+2];
         int exc = 0 + dp[i+1];
 
         dp[i] = max(inc,exc);
         }
-
         return dp[0];
-        
-
         }
     
     int rob(vector<int>& nums) {
