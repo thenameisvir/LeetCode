d606da0c3d20887c69ea8d7744465b440017ce23
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-08 14:38:55 +0530

    Time: 0 ms (100%), Space: 40.2 MB (90.69%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -2,17 +2,26 @@ class Solution {
     public int fib(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-
-       return fibb(n,dp); 
+        return tabu(n,dp); 
 
     }
 
-    int fibb(int n, int[] dp){
+    int memo(int n, int[] dp){
         if(n==1 || n==0){
             return n;
         }
 
         if(dp[n]!=-1) return dp[n];
         return dp[n] = fib(n-1)+fib(n-2);
+    }
+
+    int tabu(int n,int[] dp){
+        if(n==1 || n==0) return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
