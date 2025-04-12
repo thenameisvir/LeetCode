a5b4d8229a00d89469650bf3152ed8bcdc9f6dbe
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-12 16:18:54 +0530

    Time: 3 ms (90.15%), Space: 16 MB (14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -26,9 +26,41 @@ public:
         return dp[i][j];
 
     }
+    int tab(string word1, string word2){
+    vector<vector<int>>dp(word1.length() + 1, vector<int>(word2.length()+1,-1));
+    
+    for(int j = 0; j <= word2.length(); j++) {
+    dp[word1.length()][j] = word2.length() - j;
+}
+for(int i = 0; i <= word1.length(); i++) {
+    dp[i][word2.length()] = word1.length() - i;
+}
 
+    
+    for(int i = word1.length()-1;i>=0;i--){
+        for(int j = word2.length()-1;j>=0;j--){
+            int ans = 0;
+        if(word1[i]==word2[j]){
+            dp[i][j] = 0 + dp[i+1][j+1];
+        }
+        else{
+            int op1 = 1 + dp[i+1][j+1];
+            int op2 = 1 + dp[i][j+1];
+            int op3 = 1 + dp[i+1][j];
+
+            dp[i][j] = min(op1,min(op2,op3));
+        }
+
+        
+        }
+    }
+
+    return dp[0][0];
+
+
+    }
     int minDistance(string word1, string word2) {
         vector<vector<int>>dp(word1.length() + 1, vector<int>(word2.length()+1,-1));
-        return check(word1,word2,0,0,dp);
+        return tab(word1,word2);
     }
 };
