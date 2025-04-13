52f54e92c6153d2418234b8a5a96b7fd35b0d884
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-13 13:16:06 +0530

    Time: 619 ms (5.36%), Space: 296.5 MB (18.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int>ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]>ans.back()){
-                ans.push_back(nums[i]);
-            }
-            else{
-                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+    int usingRec(vector<int>& nums,int curr,int prev){
+        if(curr>=nums.size()) return 0;
 
-                ans[index] = nums[i];
-            }
+        // calculate one ans only 
+
+        int inc = 0, exc = 0;
+        if(prev==-1 || nums[curr]>nums[prev]){
+            inc = 1 + usingRec(nums,curr+1,curr);
         }
+        exc = 0 + usingRec(nums,curr+1,prev);
 
-        return ans.size();
+        return max(inc,exc);
+    }
+    int usingTab(vector<int>& nums,int curr,int prev,vector<vector<int>>&dp){
+        if(curr>=nums.size()) return 0;
+
+        if(dp[curr][prev+1] != -5) return dp[curr][prev+1];
+
+        int inc = 0, exc = 0;
+        if(prev==-1 || nums[curr]>nums[prev]){
+            inc = 1 + usingTab(nums,curr+1,curr,dp);
+        }
+        exc = 0 + usingTab(nums,curr+1,prev,dp);
+
+        dp[curr][prev+1] = max(inc, exc);
+return dp[curr][prev+1];
+
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-5));
+        int ans  = usingTab(nums,0,-1,dp); return ans;
     }
 };
