53c0d7c9aac6ceb8112416957deca2d979c1c7cd
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-14 13:37:30 +0530

    Time: 4 ms (1.43%), Space: 9.9 MB (99.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
-    void sortColors(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+    void sortColors(vector<int>& nums) {
+        int i = 0;     // Pointer for current element
+        int j = 0;     // Pointer for next position of 0
+        int l = nums.size() - 1; // Pointer for next position of 2
         
+        while (i <= l) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            } else if (nums[i] == 2) {
+                swap(nums[i], nums[l]);
+                l--;
+            } else {
+                i++;
+            }
+        }
     }
 };
