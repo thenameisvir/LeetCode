d26075fa69e81c18ae70b81ff07165894ffc2920
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-14 16:35:40 +0530

    Time: 0 ms (100%), Space: 71.7 MB (53.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int sum = 0;
         int maxi = nums[0];
-        int curr = 0;
         for(int i=0;i<nums.size();i++){
-            // starting me ek condition check lagaunga
-            if(curr<0){
-                // agar mera curr 0 se chota hai toh usko reset karde
-                curr = nums[i];
-            }
-            else{
-                curr +=nums[i];
-            }
+            // sabse pehle sum me include karo
+            sum +=nums[i];
+            maxi = max(maxi,sum);
 
-            // ab jab ye kar hi dia hai toh max ko bhi update kardo
-
-            maxi = max(maxi,curr);
+            if(sum<0) sum = 0;
         }
 
         return maxi;
