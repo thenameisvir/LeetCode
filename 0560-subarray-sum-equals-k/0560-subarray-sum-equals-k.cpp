cd6677053fd28abe3d65d7474d7b4be7bf529e9a
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-28 17:14:40 +0530

    Time: 37 ms (82.89%), Space: 46.8 MB (40.06%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
-        int count  =  0;
-         for(int i=0;i<arr.size();i++){
-           int sum = 0;
-           for(int j=i;j<arr.size();j++){
-            //    if(arr[j]==0) return true;
-               sum+=arr[j];
-               if(sum==k) count++;
-           }
-       }
+        int n = arr.size();
+        int count = 0;
+        vector<int>prefixSum(n,0);
 
-       return count;
+        prefixSum[0] = arr[0];
+        
+        // step 1
+
+        for(int i=1;i<n;i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+
+        // step 2
+        unordered_map<int,int>m;
+        for(int j=0;j<n;j++){
+            if(prefixSum[j]==k) count++;
+
+            int val = prefixSum[j]-k;
+            if(m.find(val)!=m.end()){
+                count+= m[val];
+            }
+
+            if(m.find(prefixSum[j])==m.end()){
+                m[prefixSum[j]] = 0;
+            }
+
+            m[prefixSum[j]]++;
+        }
+
+        return count;
+
+
+
+
+
+
     }
 };
