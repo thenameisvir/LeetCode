00e9b68363c0805a2a35de10988cb836610c036c
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-23 15:19:32 +0530

    Time: 2 ms (26.88%), Space: 9.5 MB (40.92%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,56 +1,53 @@
 class Solution {
 public:
     string decodeString(string str) {
-        stack<string> st1;
-        stack<long long int> st2;
+        
+        stack<string>st1;
+        stack<int>st2;
 
-        for (int i = 0; i < str.size(); i++) {
+        for(int i=0;i<str.size();i++){
             string temp = "";
-            long long int num = 0;
-
-            if (str[i] >= '0' && str[i] <= '9') {
-                while (i < str.size() && str[i] >= '0' && str[i] <= '9') {
-                    int a = str[i] - '0';
-                    num = (num * 10) + a;
-                    i++; // ✅ important
+            // integer hua toh 
+            int num = 0;
+            if(str[i]>='0' && str[i]<='9'){
+                while(i<str.size() && str[i]>='0' && str[i]<='9'){
+                    int a = str[i]-'0';
+                    num = (num*10) + a;
+                    i++;
                 }
-                i--; // ✅ to balance for-loop's i++
+                i--;
                 st2.push(num);
             }
-            else if (str[i] == '[' || (str[i] >= 'a' && str[i] <= 'z')) {
-                st1.push(string(1, str[i]));
+            else if(str[i]=='[' || str[i]>='a' && str[i]<='z'){
+                st1.push(string(1,str[i]));
             }
-            else if (str[i] == ']') {
-                while (!st1.empty() && st1.top() >= "a" && st1.top() <= "z") {
-                    temp = st1.top() + temp;
-                    st1.pop();
-                }
+            else if(str[i]==']'){
+                
+               while(!st1.empty() && st1.top() >= "a" && st1.top() <= "z"){
+        temp = st1.top() + temp;
+        st1.pop();
+    }
+    st1.pop(); // remove '['
 
-                if (!st1.empty() && st1.top() == "[") {
-                    st1.pop();
-                }
+    int t = st2.top(); st2.pop();
+    string temp2 = "";
+    while(t--) temp2 += temp;
 
-                string temp2 = "";
-                int a = 0;
-                if (!st2.empty()) {
-                    a = st2.top();
-                    st2.pop();
-                }
+    st1.push(temp2);
 
-                for (int j = 0; j < a; j++) {
-                    temp2 += temp;
-                }
-
-                st1.push(temp2);
             }
+
+            
+            
         }
 
         string ans = "";
-        while (!st1.empty()) {
+        while(!st1.empty()){
             ans = st1.top() + ans;
             st1.pop();
         }
 
         return ans;
+
     }
 };
