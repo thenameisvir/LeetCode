c4377dfa18e7e5556d5261e7f71c8ff27df32944
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-24 15:50:04 +0530

    Time: 4 ms (36.54%), Space: 12.2 MB (11.71%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
-    void buildAns(stack<string>&s, string&ans){
-
-        if(s.empty()){
-            return;
-        }
-
-        string minPath = s.top(); s.pop();
-        buildAns(s,ans);
-
-
-        ans+=minPath;
-        
-
-
-    }
     string simplifyPath(string path) {
-        stack<string>s;
-        int i = 0;
-        while(i<path.size()){
-            int start = i;
-            int end = i+1;
+        stack<string> st;
+        string temp;
+        stringstream ss(path);
 
-            while(end<path.size() && path[end] != '/'){
-                end++;
+        while (getline(ss, temp, '/')) {
+            if (temp == "" || temp == ".") continue;
+            if (temp == "..") {
+                if (!st.empty()) st.pop();
+            } else {
+                st.push(temp);
             }
-
-            string minPath = path.substr(start,end-start);
-            i = end;
-            if(minPath=="/" || minPath=="/."){
-                continue;
-            }
-
-            if(minPath!="/.."){
-                s.push(minPath);
-            }
-            else if(!s.empty()){
-                s.pop();
-            }
-
         }
 
-        string ans = s.empty()? "/" : "";
-        buildAns(s,ans);
+        string res = "";
+        while (!st.empty()) {
+            res = "/" + st.top() + res;
+            st.pop();
+        }
 
-        return ans;
+        return res.empty() ? "/" : res;
     }
 };
