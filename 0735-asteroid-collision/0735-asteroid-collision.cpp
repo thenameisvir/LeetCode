e5ad362859d0a0d6fad8c9c8c223943fd70a3941
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-25 15:10:35 +0530

    Time: 2 ms (35.5%), Space: 22.2 MB (41.64%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& arr) {
-        stack<int> st;
-
-        // for (int i = 0; i < arr.size(); i++) {
-        //     int curr = arr[i];
-        //     bool destroyed = false;
-
-        //     // Check for collision only if `curr` is negative
-        //     while (!st.empty() && curr < 0 && st.top() > 0) {
-        //         if (abs(curr) > st.top()) {
-        //             st.pop();
-        //         } else if (abs(curr) == st.top()) {
-        //             st.pop();
-        //             destroyed = true;
-        //             break;
-        //         } else {
-        //             destroyed = true;
-        //             break;
-        //         }
-        //     }
-
-        //     if (!destroyed) {
-        //         st.push(curr);
-        //     }
-        // }
-
-
+        stack<int>st;
         for(int i=0;i<arr.size();i++){
             int curr = arr[i];
-
             bool destroyed = false;
-
-            while(!st.empty() && curr<0 && st.top()>0){
+            while(!st.empty() && st.top()>0 && curr<0){
                 if(abs(curr)>st.top()){
                     st.pop();
                 }
@@ -47,19 +20,19 @@ public:
                 }
             }
 
-
             if(!destroyed){
                 st.push(curr);
             }
         }
 
-        // Convert stack to vector using a traditional for loop
-        vector<int> result(st.size());
-        for (int i = st.size() - 1; i >= 0; --i) {
-            result[i] = st.top();
-            st.pop();
+
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());st.pop();
         }
 
-        return result;
+        reverse(ans.begin(),ans.end());
+
+        return ans;
     }
 };
