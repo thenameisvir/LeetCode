f600027d494295112b5964b59792e672ab8c6dc8
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-25 11:03:21 +0530

    Time: 3 ms (59.2%), Space: 11.9 MB (31.39%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<string> stack;
-        int n = path.size();
-        for(int i = 0; i < n; ) {
-            // skip all '/'
-            while(i < n && path[i] == '/') i++;
-
-            // get the next component
+        stack<string>st;
+        int i = 0;
+        while(i<path.size()){
             int start = i;
-            while(i < n && path[i] != '/') i++;
-            string part = path.substr(start, i - start);
-
-            if(part == "" || part == ".") {
-                // do nothing, skip
-            } else if(part == "..") {
-                if(!stack.empty()) stack.pop_back();
-            } else {
-                // normal directory name
-                stack.push_back(part);
+            int end = i+1;
+            while(end<path.size() && path[end]!='/'){
+                end++;
+            }
+            i = end;
+            string temp = path.substr(start,end-start);
+            if(temp=="/" || temp=="/."){
+                continue;
+            }
+            else if(temp!="/.."){
+                st.push(temp);
+            }
+            else if(!st.empty() && temp=="/.."){
+                st.pop();
             }
         }
 
-        // build result
-        string ans = "/";
-        for(int i = 0; i < stack.size(); i++) {
-            ans += stack[i];
-            if(i != stack.size() - 1) ans += "/";
+        string ans = "";
+        if(st.empty()){
+            ans.push_back('/');
+            return ans;
+        }
+
+        while(!st.empty()){
+            string p = st.top();
+            ans = p + ans;
+            st.pop();
         }
 
         return ans;
