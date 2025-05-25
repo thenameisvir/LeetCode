7b084f1b27ed4fa866251d75e135aca3c8f24475
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-25 12:43:00 +0530

    Time: 56 ms (9.61%), Space: 85.8 MB (37.19%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,55 +4,49 @@ public:
         vector<int>ans(arr.size());
         stack<int>st;
         st.push(-1);
-
         for(int i=0;i<arr.size();i++){
             while(st.top()!=-1 && arr[i]<=arr[st.top()]){
                 st.pop();
             }
-
-            ans[i] = st.top();
-            st.push(i);
+        ans[i] = st.top();
+        st.push(i);
         }
-
         return ans;
     }
     vector<int>solve2(vector<int>& arr){
         vector<int>ans(arr.size());
         stack<int>st;
         st.push(-1);
-
         for(int i=arr.size()-1;i>=0;i--){
             while(st.top()!=-1 && arr[i]<=arr[st.top()]){
                 st.pop();
             }
-
-            ans[i] = st.top();
-            st.push(i);
+        ans[i] = st.top();
+        st.push(i);
         }
-
         return ans;
     }
     int largestRectangleArea(vector<int>& arr) {
         vector<int>prev = solve1(arr);
         vector<int>next = solve2(arr);
-        // yaha par next ke funde ko bacha hai 
+
+        int maxArea = INT_MIN;
+        // ek special condition hoti hai yaha par 
         for(int i=0;i<next.size();i++){
             if(next[i]==-1){
                 next[i] = next.size();
             }
         }
 
-        int maxArea = INT_MIN;
-
         for(int i=0;i<arr.size();i++){
             int length = arr[i];
             int breadth = next[i]-prev[i]-1;
-
             int area = length*breadth;
 
-            maxArea = max(area,maxArea);
+            maxArea = max(maxArea,area);
         }
 
         return maxArea;
+
     }
 };
