98dfd3cd45691fbc82ed2c895a48313bc807b3c0
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-30 11:47:18 +0530

    Time: 95 ms (5.47%), Space: 130.6 MB (5.33%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,33 +1,56 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        // bhot important hai bhula mt kar bkl
-        // ye wali approach achi hai isko bhulna mt 
+        // int current = 0;
+        // int totalfuel = 0;
+        // int totalcost = 0;
+        // int start = 0;
+        // for(int i=0;i<gas.size();i++){
+        //     totalfuel+=gas[i];
+        // }
+        // for(int i=0;i<gas.size();i++){
+        //     totalcost+=cost[i];
+        // }
+        // if(totalfuel<totalcost){
+        //     return -1;
+        // }
+
+        // for(int i=0;i<gas.size();i++){
+        //     current+=(gas[i]-cost[i]);
+        //     if(current<0){
+        //         start = i+1;
+        //         current = 0;
+
+        //     }
+        // }
+        // return start;
 
 
-        int deficit = 0;
-        int balance = 0;
+
+
         int start = 0;
-
+        int totalfuel = 0;
+        int totalcost = 0;
+        int current = 0;
         for(int i=0;i<gas.size();i++){
-            balance = balance + gas[i] - cost[i];
-
-            if(balance<0){
-                deficit+=abs(balance);
-                start=i+1;
-                balance = 0;
-            }
+            totalfuel+=gas[i];
+        }
+        for(int i=0;i<gas.size();i++){
+            totalcost+=cost[i];
         }
 
-        if(balance-deficit>=0){
-            return start;
-        }
-        else{
+        if(totalfuel<totalcost){
             return -1;
         }
 
-        
+        for(int i=0;i<gas.size();i++){
+            current+=(gas[i]-cost[i]);
+            if(current<0){
+                start = i+1;
+                current = 0;
+            }
+        }
 
-
+        return start;
     }
 };
