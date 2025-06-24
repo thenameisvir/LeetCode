043400bc48e88f527c9c91c8a21893d90c8aaa32
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-06-25 00:39:46 +0530

    Time: 0 ms (100%), Space: 10.9 MB (64.45%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,65 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        // vector<int> ans;
-        // TreeNode* curr = root;
-
-        // while (curr) {
-        //     if (curr->left == NULL) {
-        //         ans.push_back(curr->val);
-        //         curr = curr->right;
-        //     } else {
-        //         TreeNode* pred = curr->left;
-
-                
-        //         while (pred->right && pred->right != curr) {
-        //             pred = pred->right;
-        //         }
-
-        //         if (pred->right == NULL) {
-                    
-        //             pred->right = curr;
-        //             curr = curr->left;
-        //         } else {
-                    
-        //             pred->right = NULL;
-        //             ans.push_back(curr->val);
-        //             curr = curr->right;
-        //         }
-        //     }
-        // }
-
-        // return ans;
-
-
-        // sbse pehle apan log ek vector banayenge 
-        vector<int>ans; 
-        // iske baad ab apan ek node banayenge
+        vector<int>ans;
         TreeNode* curr = root;
-
+        if(!root) return ans;
         while(curr){
-            if(!curr->left){
-                // agar mera left null hai to cha mudaye duniya mai to chala push karne
+            if(curr->left==NULL){
+                // push condition has been hit
                 ans.push_back(curr->val);
-                // ab iske baad idhar udhar bhi to jana padega na to mai left ko null krke aya hu
-                // toh ab right me jaunga 
                 curr = curr->right;
             }
             else{
-                // ye jo node hai pehle ek kadam left jayegi , chalo jao
                 TreeNode* pred = curr->left;
-                // ab is bkl ko bhejo khub dur
-                while(pred->right&&pred->right!=curr){
+                while(pred->right!=curr && pred->right){
                     pred = pred->right;
                 }
 
-                // ab sasura check karenge ki pred ka right null hai ya nahii
-
                 if(!pred->right){
-
                     pred->right = curr;
                     curr = curr->left;
-
                 }
                 else{
                     pred->right = NULL;
