e61972a2fcf51ecb390d555c30be431b552c1405
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-02 10:29:06 +0530

    Time: 23 ms (25.9%), Space: 65.8 MB (45.78%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -1,49 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void BuildIt(TreeNode* root,vector<int>&v){
-        if(!root){
-            return;
-        }
-
-        BuildIt(root->left,v);
-        v.push_back(root->val);
-        BuildIt(root->right,v);
+    void solve(TreeNode* root, vector<int>& ans) {
+        if(!root) return;
+        solve(root->left, ans);
+        ans.push_back(root->val);
+        solve(root->right, ans);
     }
-    TreeNode* solve(TreeNode* &moot,vector<int>v,int s,int e){
-        if(s>e) return NULL;
 
-        int m = s+(e-s)/2;
-
-        TreeNode* root = new TreeNode(v[m]);
-
-        root->left = solve(moot,v,s,m-1);
-        root->right = solve(moot,v,m+1,e);
-
-        return root;
-
+    TreeNode* okay(vector<int>& ans, int s, int e) {
+        if(s > e) return NULL;
+        int m = s + (e - s) / 2;
+        TreeNode* newNode = new TreeNode(ans[m]);
+        newNode->left = okay(ans, s, m - 1);
+        newNode->right = okay(ans, m + 1, e);
+        return newNode;
     }
+
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int>v;
-        BuildIt(root,v);
-        TreeNode* moot = NULL;
-
-        int s = 0;
-        int e = v.size()-1;
-    
-        moot = solve(moot,v,s,e);
-        return moot;
-
-
+        vector<int> ans;
+        solve(root, ans);
+        return okay(ans, 0, ans.size() - 1);
     }
 };
