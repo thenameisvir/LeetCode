6a8026de2655c2faa33ed9b41976ea7f37342fc1
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-10 22:49:28 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.63%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,77 +1,71 @@
-#include <string>
-#include <queue>
-#include <vector>
-using namespace std;
-
-class Info {
-public:
-    char val;
-    int count;
-
-    Info(char ch, int cnt) {
-        this->val = ch;
-        this->count = cnt;
-    }
-};
-
-class compare {
-public:
-    bool operator()(Info a, Info b) {
-        return a.count < b.count; // Max heap based on count
-    }
-};
-
 class Solution {
 public:
+    class Info{
+        public:
+        int count;
+        char ch;
+
+        Info(char ch,int count){
+            this->count = count;
+            this->ch = ch;
+        }
+    };
+    class compare{
+        public:
+        bool operator()(Info a,Info b){
+            return a.count<b.count;
+        }
+    };
     string reorganizeString(string s) {
-        int freq[26] = {0};
-        // Count frequency of all characters in the string
-        for (int i = 0; i < s.length(); i++) {
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++){
             char ch = s[i];
-            freq[ch - 'a']++;
-        }
+            freq[ch-'a']++;
+        }    
 
-        // Push all characters into a max heap
-        priority_queue<Info, vector<Info>, compare> pq;
-
-        for (int i = 0; i < 26; i++) {
+        // yaha tak frequency store ho chuki hai 
+        priority_queue<Info,vector<Info>,compare>pq;
+        for(int i=0;i<26;i++){
             if (freq[i] > 0) {
-                pq.push(Info(i + 'a', freq[i]));
-            }
+    Info newNode = Info('a' + i, freq[i]);
+    pq.push(newNode);
+}
         }
 
-        string ans = "";
-        while (pq.size() > 1) {
-            Info first = pq.top();
-            pq.pop();
-            Info second = pq.top();
-            pq.pop();
+        string str = "";
+        while(pq.size()>1){
 
-            ans.push_back(first.val);
+            Info first = pq.top();pq.pop();
+            Info second = pq.top();pq.pop();
+
+            str.push_back(first.ch);
+            str.push_back(second.ch);
+
             first.count--;
-            ans.push_back(second.val);
             second.count--;
 
-            if (first.count > 0) {
+            if(first.count>0){
                 pq.push(first);
             }
-            if (second.count > 0) {
+            if(second.count>0){
                 pq.push(second);
             }
+
+
         }
 
-        // Handle the case where one element is left
-        if (pq.size() == 1) {
-            Info last = pq.top();
-            pq.pop();
-
-            if (last.count > 1) {
-                // Not possible to reorganize
+        if(pq.size()==1){
+            Info temp = pq.top();pq.pop();
+            if(temp.count>1){
                 return "";
             }
-            ans.push_back(last.val);
+            else{
+                str.push_back(temp.ch);
+            }
         }
 
-        return ans;
+        return str;
+
+
     }
 };
