1e8ad71cce038b526c8dbc7880b4c40718567b63
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-15 17:15:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int usingRec(vector<int>& arr,int i,vector<int>&dp){
-        if(i>=arr.size()){
-            return 0;
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        vector<int> ans(n, 0);
+        ans[n - 1] = nums[n - 1];
+        ans[n - 2] = max(nums[n - 2], nums[n - 1]);
+
+        for(int i = n - 3; i >= 0; i--) {
+            ans[i] = max(nums[i] + ans[i + 2], ans[i + 1]);
         }
 
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-
-        int inc = arr[i] + usingRec(arr,i+2,dp);
-        int exc = 0 + usingRec(arr,i+1,dp);
-
-        return dp[i] = max(inc,exc);
-
-    }
-    int rob(vector<int>& arr) {
-        vector<int>dp(arr.size()+1,-1);
-        return usingRec(arr,0,dp);
+        return ans[0];
     }
 };
