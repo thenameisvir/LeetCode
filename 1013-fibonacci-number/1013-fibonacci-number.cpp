512da87f4afbe1e0ae22ce356e2bf51194bc0cb9
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-15 11:46:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (21.74%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    void solve(vector<int>& ans, int n) {
-        // Base cases
-        ans[0] = 0;
-        if (n >= 1) ans[1] = 1;
-
-        // Fill the dp array using bottom-up approach
-        for (int i = 2; i <= n; i++) {
-            ans[i] = ans[i - 1] + ans[i - 2];
-        }
-    }
-
     int fib(int n) {
-        if (n == 0) return 0;  // Edge case
-        vector<int> ans(n + 1, 0);
-        solve(ans, n);
-        return ans[n];
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1;
+        int i = 2;
+        while (i <= n) {
+            int c = a + b;
+            a = b;
+            b = c;
+            i++;
+        }
+        return b;
     }
 };
