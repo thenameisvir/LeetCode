b14d26dc3ec27f3849649b29929e0d8ea606e9f4
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-15 15:53:12 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int i,vector<int>&dp){
-        // base case would be 
-        if(i>=nums.size()) return 0;
+    int usingRec(vector<int>& arr,int i,vector<int>&dp){
+        if(i>=arr.size()){
+            return 0;
+        }
 
-        // main case would be
-        if(dp[i]!=INT_MIN) return dp[i];
-        int inc = nums[i] + solve(nums,i+2,dp);
-        int exc = 0 + solve(nums,i+1,dp);
+        if(dp[i]!=-1){
+            return dp[i];
+        }
 
-        dp[i] =  max(inc,exc);
+        int inc = arr[i] + usingRec(arr,i+2,dp);
+        int exc = 0 + usingRec(arr,i+1,dp);
 
-        return dp[i];
+        return dp[i] = max(inc,exc);
+
     }
-    int tabulation(vector<int>& nums){
-        int n = nums.size();
-        vector<int>dp(n+1,0);
-
-        dp[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-        int tempAns = 0;
-        
-        int inc = nums[i] + dp[i+2];
-        int exc = 0 + dp[i+1];
-
-        dp[i] = max(inc,exc);
-        }
-        return dp[0];
-        }
-    
-    int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,INT_MIN);
-        int ans = tabulation(nums);
-        return ans;
+    int rob(vector<int>& arr) {
+        vector<int>dp(arr.size()+1,-1);
+        return usingRec(arr,0,dp);
     }
 };
