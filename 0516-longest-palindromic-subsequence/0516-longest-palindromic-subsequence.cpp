75afa573ede27ba5c429e8d88ee57e3860ae4d7a
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-17 09:41:07 +0530

    Time: 99 ms (10.18%), Space: 75.3 MB (76.77%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    int usingTab(string a, string b){
-    vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
-    // fill something into the dp according to the base cases 
-    // here we took the size of the 2D matrix increasd because we need to attain indexes in tabulation
-    // so to not get out of bound this is mandatory
-    for(int i=a.length()-1;i>=0;i--){
-        for(int j=b.length()-1;j>=0;j--){
-        int ans = 0;
-        // if my both of the char are matching
-        if(a[i]==b[j]){
-            ans = 1 + dp[i+1][j+1];
+    int space(string str1,string str2){
+        int n = str1.size()-1;
+        vector<int>prev(n+2,0);
+        for(int i=n;i>=0;i--){
+            vector<int>curr(n+2,0);
+            for(int j=n;j>=0;j--){
+                int ans = 0;
+                if(str1[i]==str2[j]){
+                    ans = 1 + prev[j+1];
+                }
+                int ans2 = prev[j];
+                int ans3 = curr[j+1];
+
+                curr[j] = max(ans,max(ans2,ans3));
+            }
+
+            prev = curr;
         }
-        else{
-            ans = 0 + max(dp[i+1][j],dp[i][j+1]);
-        }
-        dp[i][j] = ans;
-        }
 
-
-    }
-
-    return dp[0][0];
-
-
+        return prev[0];
     }
     int longestPalindromeSubseq(string s) {
-        string a = s;
-        string b = s;
-        reverse(b.begin(),b.end());
-        int anss = usingTab(a,b); return anss;
+        string str1 = s;
+        reverse(s.begin(),s.end());
+        string str2 = s;
+        return space(str1,str2);
     }
 };
