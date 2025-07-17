4859373b0ce89c4d3760650b85fed9dbbda219cc
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-17 09:28:14 +0530

    Time: 33 ms (32.47%), Space: 45.2 MB (6.03%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -42,10 +42,33 @@ public:
     return dp[0][0];
 
     }
+
+    int space(string str1, string str2){
+        vector<int>prev(str2.size()+1,0);
+        int n = str1.size()-1;
+        int m = str2.size()-1;
+        for(int i=n;i>=0;i--){
+            vector<int>curr(m+2,0);
+            for(int j=m;j>=0;j--){
+                int ans1 = 0;
+                if(str1[i]==str2[j]){
+                    ans1 = 1 + prev[j+1];
+                }
+                int ans2 = curr[j+1];
+                int ans3 = prev[j];
+
+                curr[j] = max(ans1,max(ans2,ans3));
+            }
+            prev = curr;
+        }
+
+        return prev[0];
+
+    }
     int longestCommonSubsequence(string str1, string str2) {
         vector<vector<int>>dp(str1.size()+1,vector<int>(str2.size()+1,-1));
         int i = 0;
         int j = 0;
-        return tab(str1,str2);
+        return space(str1,str2);
     }
 };
