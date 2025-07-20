5129057dc3cea19ccdc440bbabaf7219a0cc7502
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-20 23:42:05 +0530

    Time: 394 ms (29.15%), Space: 576.2 MB (6.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -17,9 +17,28 @@ public:
 
         return dp[curr][prev+1] =  max(inc,exc);
     }
+    int tab(vector<int>&nums){
+        int n = nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                int inc = 0;
+            if(j==-1 || nums[i]>nums[j]){
+                inc = 1 + dp[i+1][i+1];
+            }
+            int exc = 0 + dp[i+1][j+1];
+
+             dp[i][j+1] =  max(inc,exc);
+            }
+        }
+
+    return dp[0][0];
+
+    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        return solve(nums,0,-1,dp);
+        return tab(nums);
     }
 };
