75b0996db6273e6be481c7f082058ab6c1536164
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-21 17:07:15 +0530

    Time: 194 ms (12.96%), Space: 115.1 MB (14.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -17,6 +17,35 @@ public:
 
         return dp[i][target] = inc||exc;
     }
+
+    bool tab(vector<int>& nums){
+        int sum = 0;
+        for(int i=0;i<nums.size();i++) sum+=nums[i];
+
+        if(sum%2!=0) return false;
+
+        int target = sum/2;
+
+        int n = nums.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(target+1,false));
+
+        for(int i = 0; i <= n; i++) dp[i][0] = true;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=target;j++){
+            bool inc = false;
+            if(j - nums[i] >= 0)
+                inc = dp[i + 1][j - nums[i]];
+            bool exc = dp[i + 1][j];
+
+            dp[i][j] = inc||exc;
+            }
+        }
+
+        return dp[0][target];
+
+    }
+
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int i=0;i<nums.size();i++) sum+=nums[i];
@@ -28,6 +57,6 @@ public:
         int n = nums.size();
         vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
 
-        return solve(nums,0,target,dp);
+        return tab(nums);
     }
 };
