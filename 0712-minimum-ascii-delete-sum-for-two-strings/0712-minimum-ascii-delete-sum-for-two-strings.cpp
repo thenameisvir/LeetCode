fb40e18dbfe3b5c696c00c4938eb29fc5d93a749
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-25 11:15:40 +0530

    Time: 37 ms (27.17%), Space: 18.3 MB (27.29%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int makichu(string &s1, string &s2, int i, int j) {
-        if (i >= s1.length()) {
+    int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+        // base case baad me 
+        if(i>=s1.size() && j<s2.size()){
             int sum = 0;
-            while (j < s2.length()) sum += s2[j++];
+            while(j<s2.size()){
+                sum+=int(s2[j]);
+                j++;
+                
+            }
             return sum;
         }
-        if (j >= s2.length()) {
-            int sum = 0;
-            while (i < s1.length()) sum += s1[i++];
-            return sum;
-        }
-        if (dp[i][j] != -1) return dp[i][j]; // DP check
 
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = makichu(s1, s2, i + 1, j + 1);
-        } else {
-            int option1 = s1[i] + makichu(s1, s2, i + 1, j);
-            int option2 = s2[j] + makichu(s1, s2, i, j + 1);
-            return dp[i][j] = min(option1, option2);
+        if(j>=s2.size() && i<s1.size()){
+            int sum = 0;
+            while(i<s1.size()){
+                sum+=int(s1[i]);
+               i++;
+            }
+             return sum;
         }
+
+        if(i>=s1.size() && j>=s2.size()) return 0;
+
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(s1[i]==s2[j]){
+            return solve(s1,s2,i+1,j+1,dp);
+        }
+
+        int op1 = int(s1[i]) + solve(s1,s2,i+1,j,dp);
+        int op2 = int(s2[j]) + solve(s1,s2,i,j+1,dp);
+
+        return dp[i][j] = min(op1,op2);
     }
-
     int minimumDeleteSum(string s1, string s2) {
-        dp.resize(s1.length(), vector<int>(s2.length(), -1));
-        return makichu(s1, s2, 0, 0);
+        vector<vector<int>>dp(s1.size() + 1,vector<int>(s2.size()+1,-1));
+        return solve(s1,s2,0,0,dp);
     }
 };
