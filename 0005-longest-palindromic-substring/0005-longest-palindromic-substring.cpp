d45513cbf8a6208298759e96f6a0b530b95d846f
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-25 13:58:30 +0530

    Time: 380 ms (15.3%), Space: 291.9 MB (9.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    int start = 0, maxLen = 1;
+    int start = 0;
+    int maxLen = 1;
 
-    bool isPalindrome(int i, int j, string &s, vector<vector<int>>& dp) {
-        // base cases
-        if (i >= j) return true;
+    bool solve(int i, int j, string &s, vector<vector<int>> &dp) {
+        if(i >= j) return true;
 
-        // memoized
-        if (dp[i][j] != -1)
-            return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
-        if (s[i] == s[j]) {
-            // check inner substring
-            if (isPalindrome(i + 1, j - 1, s, dp)) {
-                // update longest palin substring if needed
-                if (j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
-                    start = i;
-                }
-                return dp[i][j] = true;
+        bool flag = false;
+        if(s[i] == s[j]) {
+            flag = solve(i+1, j-1, s, dp);
+        }
+
+        if(flag) {
+            int len = j - i + 1;
+            if(len > maxLen) {
+                start = i;
+                maxLen = len;
             }
         }
 
-        return dp[i][j] = false;
+        return dp[i][j] = flag;
     }
 
     string longestPalindrome(string s) {
-        int n = s.length();
+        int n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                isPalindrome(i, j, s, dp);
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                solve(i, j, s, dp);
             }
         }
 
