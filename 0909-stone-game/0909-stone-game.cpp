7d1c70e1dd36a7a62401d7bdf2275018fa937802
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-31 13:17:26 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.73%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,43 +1,6 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& arr) {
-        // priority_queue<int>pq;
-        if(arr.size()==1) return true;
-
-        int n = arr.size()-1;
-        // pq.push(arr[0]);
-        // pq.push(arr[n]);
-
-        int alice = 0, bob = 0;
-        int i = 0;
-        bool flag = true;
-        while(i<n){
-            int a = arr[i];
-            int b = arr[n];
-            if(flag){
-                if(a>b){
-                    alice+=a;
-                    i++;
-                }
-                else{
-                    alice+=b;
-                    n--;
-                }
-                flag = false;
-            }
-            else{
-                if(a>b){
-                    alice+=a;
-                    i++;
-                }
-                else{
-                    alice+=b;
-                    n--;
-                }
-                flag = true;
-            }
-        }
-
-        return alice>bob;
+    bool stoneGame(vector<int>& piles) {
+        return true;
     }
 };
