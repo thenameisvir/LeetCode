d6511691849147d907603a475ed2352c85d4081d
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-08-20 15:58:54 +0530

    Time: 0 ms (100%), Space: 114.4 MB (5.02%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
+    // Ye sirf nth natural number sum deta hai: 1 + 2 + ... + n
+    long long solve(int n){
+        if(n==0 || n==1) return n;
+        return solve(n-1) + n;
+    }
+
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long cnt = 0, streak = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                streak++;
-                cnt += streak;
-            } else {
-                streak = 0;
+        long long ans = 0;
+
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0){
+                int j = i;
+                while(j<n && nums[j]==0){
+                    j++;
+                }
+
+                // ab block [i...j-1] me sirf 0 hai
+                int len = j - i;
+
+                // us block ke subarrays of zero ka count
+                ans += solve(len);
+
+                // i ko aage le jao directly
+                i = j-1;
             }
         }
-        return cnt;
+
+        return ans;
     }
 };
