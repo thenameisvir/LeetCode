29da4993e5c8345d40b97a79bbfed2b4bae5a0bb
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-09-02 21:28:12 +0530

    Time: 1525 ms (24.74%), Space: 0B (100%) - LeetHub

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+select customer_id, count(*) as count_no_trans
+from (
+select v.customer_id, t.transaction_id
+from Visits v left join Transactions t
+on v.visit_id = t.visit_id ) as temp
+where transaction_id is null
+group by customer_id;
